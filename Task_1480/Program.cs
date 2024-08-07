bf2b35bef4f39c7878bc0f03bb3cea286780f215
Sergey Korepanov <igzhestik@mail.ru>
2024-08-07 21:08:53 +0500

    Task 1480. Running Sum of 1d Array

---
+++ Task_1480/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            result[i] = sum;
+        }
+
+        return result;
+    }
+}
