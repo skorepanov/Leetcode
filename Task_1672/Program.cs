d37f60de6b28e399dc60532e733290455e376ff2
Sergey Korepanov <igzhestik@mail.ru>
2024-08-07 21:15:04 +0500

    Task 1672. Richest Customer Wealth

---
+++ Task_1672/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        var maxWealth = int.MinValue;
+
+        foreach (var accountSums in accounts)
+        {
+            var accountTotalSum = accountSums.Sum();
+            maxWealth = Math.Max(maxWealth, accountTotalSum);
+        }
+
+        return maxWealth;
+    }
+}
