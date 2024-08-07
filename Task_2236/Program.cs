b4175e5e46dd5347a45115438c1aaa64ceaa1c28
Sergey Korepanov <igzhestik@mail.ru>
2024-08-07 20:29:31 +0500

    Task 2236. Root Equals Sum of Children

---
+++ Task_2236/Program.cs
@@ -0,0 +1,22 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool CheckTree(TreeNode root)
+    {
+        var sum = root.left.val + root.right.val;
+        return root.val == sum;
+    }
+}
