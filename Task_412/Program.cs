c1bb5837c201d196afdf70b59c3130f5095a9020
Sergey Korepanov <igzhestik@mail.ru>
2024-08-08 17:49:52 +0500

    Task 412. Fizz Buzz

---
+++ Task_412/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        var answers = new List<string>();
+
+        for (var i = 1; i <= n; i++)
+        {
+            var isDivisibleBy3 = i % 3 == 0;
+            var isDivisibleBy5 = i % 5 == 0;
+
+            if (isDivisibleBy3 && isDivisibleBy5)
+            {
+                answers.Add("FizzBuzz");
+            }
+            else if (isDivisibleBy3)
+            {
+                answers.Add("Fizz");
+            }
+            else if (isDivisibleBy5)
+            {
+                answers.Add("Buzz");
+            }
+            else
+            {
+                answers.Add(i.ToString());
+            }
+        }
+
+        return answers;
+    }
+}
