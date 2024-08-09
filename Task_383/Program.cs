27714d3b1720368da2f8f5a836c834c3bbf89beb
Sergey Korepanov <igzhestik@mail.ru>
2024-08-09 16:09:40 +0500

    Task 383. Ransom Note

---
+++ Task_383/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var symbols = new Dictionary<char, int>();
+
+        foreach (var symbol in magazine)
+        {
+            if (symbols.ContainsKey(symbol))
+            {
+                symbols[symbol]++;
+            }
+            else
+            {
+                symbols.Add(symbol, 1);
+            }
+        }
+
+        foreach (var symbol in ransomNote)
+        {
+            if (symbols.ContainsKey(symbol) && symbols[symbol] > 0)
+            {
+                symbols[symbol]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
