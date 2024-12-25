d30085788dc9799526817da443547c7843b3f9ea
Sergey Korepanov <igzhestik@mail.ru>
2024-12-25 23:13:47 +0500

    Task 54. Spiral Matrix

--- CurrentTask/Program.cs
+++ CurrentTask/Program.cs
@@ -6,29 +6,54 @@ public class Solution1
     {
         var solution = new Solution();
 
-         int[][] array1 =
-         {
-             new [] { 1, 2, 3 },
-             new [] { 4, 5, 6 },
-             new [] { 7, 8, 9 }
-         };
-        var result1 = solution.FindDiagonalOrder(array1);
+        #region Solution 54
+        //  int[][] array1 =
+        //  {
+        //      new [] { 1, 2, 3 },
+        //      new [] { 4, 5, 6 },
+        //      new [] { 7, 8, 9 }
+        //  };
+        // var result1 = solution.FindDiagonalOrder(array1);
+        //
+        //  int[][] array2 =
+        //  {
+        //      new [] { 1, 2 },
+        //      new [] { 3, 4 }
+        //  };
+        // var result2 = solution.FindDiagonalOrder(array2);
+        //
+        // int[][] array3 = { new[] { 2, 3 } };
+        // var result3 = solution.FindDiagonalOrder(array3);
+        //
+        // int[][] array4 = { new[] { 3 } , new [] { 2 } };
+        // var result4 = solution.FindDiagonalOrder(array4);
+        //
+        // int[][] array5 = { new[] { 6, 9, 7 } };
+        // var result5 = solution.FindDiagonalOrder(array5);
+        #endregion
 
-         int[][] array2 =
-         {
-             new [] { 1, 2 },
-             new [] { 3, 4 }
-         };
-        var result2 = solution.FindDiagonalOrder(array2);
+        #region Solution 54
+        // int[][] array1 =
+        // {
+        //     new [] { 1, 2, 3 },
+        //     new [] { 4, 5, 6 },
+        //     new [] { 7, 8, 9 }
+        // };
+        // var result1 = solution.SpiralOrder(array1);
 
-        int[][] array3 = { new[] { 2, 3 } };
-        var result3 = solution.FindDiagonalOrder(array3);
-
-        int[][] array4 = { new[] { 3 } , new [] { 2 } };
-        var result4 = solution.FindDiagonalOrder(array4);
-
-        int[][] array5 = { new[] { 6, 9, 7 } };
-        var result5 = solution.FindDiagonalOrder(array5);
+        // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
+        int[][] array2 =
+        {
+            new [] { 1, 2, 3, 4 },
+            new [] { 5, 6, 7, 8 },
+            new [] { 9, 10, 11, 12 },
+            new [] { 13, 14, 15, 16 },
+            new [] { 17, 18, 19, 20 },
+            new [] { 21, 22, 23, 24 }
+        };
+        // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
+        var result2 = solution.SpiralOrder(array2);
+        #endregion
     }
 
     public class ListNode
---
+++ CurrentTask/Solution_54.cs
@@ -0,0 +1,96 @@
+﻿namespace CurrentTask;
+
+public class Solution
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        if (matrix is null || matrix.Length == 0)
+        {
+            return null;
+        }
+
+        var result = new List<int>();
+        var totalCount = matrix.Length * matrix[0].Length;
+
+        var i = 0;
+        var k = 0;
+
+        var left = -1;
+        var right = matrix[0].Length;
+        var top = -1;
+        var bottom = matrix.Length;
+
+        var resultIndex = 0;
+
+        var direction = Direction.Right;
+
+        while (resultIndex < totalCount)
+        {
+            result.Add(matrix[i][k]);
+            resultIndex++;
+
+            switch (direction)
+            {
+                case Direction.Right:
+                    if (k + 1 >= right)
+                    {
+                        direction = Direction.Bottom;
+                        top++;
+                        i++;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                    break;
+                case Direction.Bottom:
+                    if (i + 1 >= bottom)
+                    {
+                        direction = Direction.Left;
+                        right--;
+                        k--;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    break;
+                case Direction.Left:
+                    if (k - 1 <= left)
+                    {
+                        direction = Direction.Up;
+                        bottom--;
+                        i--;
+                    }
+                    else
+                    {
+                        k--;
+                    }
+                    break;
+                case Direction.Up:
+                    if (i - 1 <= top)
+                    {
+                        direction = Direction.Right;
+                        left++;
+                        k++;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    private enum Direction
+    {
+        Left = 0,
+        Right = 1,
+        Up = 2,
+        Bottom = 3
+    }
+}
