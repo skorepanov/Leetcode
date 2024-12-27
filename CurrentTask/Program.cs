21cb1dd52dd38f9d6395b65d37faee2aaaaa43ae
Sergey Korepanov <igzhestik@mail.ru>
2024-12-27 19:08:47 +0500

    Task 118. Pascal's Triangle

--- CurrentTask/Program.cs
+++ CurrentTask/Program.cs
@@ -41,18 +41,23 @@ public class Solution1
         // };
         // var result1 = solution.SpiralOrder(array1);
 
-        // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
-        int[][] array2 =
-        {
-            new [] { 1, 2, 3, 4 },
-            new [] { 5, 6, 7, 8 },
-            new [] { 9, 10, 11, 12 },
-            new [] { 13, 14, 15, 16 },
-            new [] { 17, 18, 19, 20 },
-            new [] { 21, 22, 23, 24 }
-        };
-        // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
-        var result2 = solution.SpiralOrder(array2);
+
+        // // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
+        // int[][] array2 =
+        // {
+        //     new [] { 1, 2, 3, 4 },
+        //     new [] { 5, 6, 7, 8 },
+        //     new [] { 9, 10, 11, 12 },
+        //     new [] { 13, 14, 15, 16 },
+        //     new [] { 17, 18, 19, 20 },
+        //     new [] { 21, 22, 23, 24 }
+        // };
+        // // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
+        // var result2 = solution.SpiralOrder(array2);
+        #endregion
+
+        #region Solution 118
+        var result = solution.Generate(5);
         #endregion
     }
 
---
+++ CurrentTask/Solution.cs
@@ -0,0 +1,37 @@
+﻿// 118. Pascal's Triangle
+namespace CurrentTask;
+
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var triangle = new List<IList<int>>
+        {
+            new List<int> { 1 }
+        };
+
+        if (numRows == 1)
+        {
+            return triangle;
+        }
+
+        for (var i = 2; i <= numRows; i++)
+        {
+            var triangleRow = new List<int>
+            {
+                1
+            };
+
+            for (var k = 0; k < i - 2; k++)
+            {
+                var previousRow = triangle[i - 2];
+                triangleRow.Add(previousRow[k] + previousRow[k + 1]);
+            }
+
+            triangleRow.Add(1);
+            triangle.Add(triangleRow);
+        }
+
+        return triangle;
+    }
+}
