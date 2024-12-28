fbb35f8cf2e50d312adb246ade06c55fa46a0663
Sergey Korepanov <igzhestik@mail.ru>
2024-12-28 17:26:24 +0500

    Task 28. Find the Index of the First Occurrence in a String

---
+++ CurrentTask/Solution.cs
@@ -0,0 +1,10 @@
+﻿// 28. Find the Index of the First Occurrence in a String
+// namespace CurrentTask;
+//
+// public class Solution_28
+// {
+//     public int StrStr(string haystack, string needle)
+//     {
+//         return haystack.IndexOf(needle);
+//     }
+// }
