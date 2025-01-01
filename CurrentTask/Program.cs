0327e0749ec0b4d7372d794c8a9eee751fdf49ca
Sergey Korepanov <igzhestik@mail.ru>
2025-01-01 15:58:22 +0500

    Task 189. Rotate Array

--- CurrentTask/Program.cs
+++ CurrentTask/Program.cs
@@ -7,83 +7,85 @@ public class Solution1
         var solution = new Solution();
     }
 
-    //public static void Main()
-    //{
-        #region Solution 209
-        //var result = solution.MinSubArrayLen(7, new[] { 2, 3, 1, 2, 4, 3 });
-        #endregion
+    #region Solution 189
+    // var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+    // solution.Rotate(nums, 1);
+    #endregion
 
-        #region Solution 561
-        // //var nums = new int[] { 1, 4, 3, 2 };
-        // var nums = new int[] { 6, 2, 6, 5, 1, 2 };
-        // var result = solution.ArrayPairSum(nums);
-        #endregion
+    #region Solution 209
+    //var result = solution.MinSubArrayLen(7, new[] { 2, 3, 1, 2, 4, 3 });
+    #endregion
 
-        #region Solution 344
-        // var s = new [] { '1', '2', '3', '4', '5' };
-        // solution.ReverseString(s);
-        #endregion
+    #region Solution 561
+    // //var nums = new int[] { 1, 4, 3, 2 };
+    // var nums = new int[] { 6, 2, 6, 5, 1, 2 };
+    // var result = solution.ArrayPairSum(nums);
+    #endregion
 
-        #region Solution 14
-        //var result = solution.LongestCommonPrefix(new [] { "flower","flow","flight" });
-        #endregion
+    #region Solution 344
+    // var s = new [] { '1', '2', '3', '4', '5' };
+    // solution.ReverseString(s);
+    #endregion
 
-        #region Solution 57
-        //var result = solution.AddBinary("100", "110010");
-        #endregion
+    #region Solution 14
+    //var result = solution.LongestCommonPrefix(new [] { "flower","flow","flight" });
+    #endregion
 
-        #region Solution 498
-        //  int[][] array1 =
-        //  {
-        //      new [] { 1, 2, 3 },
-        //      new [] { 4, 5, 6 },
-        //      new [] { 7, 8, 9 }
-        //  };
-        // var result1 = solution.FindDiagonalOrder(array1);
-        //
-        //  int[][] array2 =
-        //  {
-        //      new [] { 1, 2 },
-        //      new [] { 3, 4 }
-        //  };
-        // var result2 = solution.FindDiagonalOrder(array2);
-        //
-        // int[][] array3 = { new[] { 2, 3 } };
-        // var result3 = solution.FindDiagonalOrder(array3);
-        //
-        // int[][] array4 = { new[] { 3 } , new [] { 2 } };
-        // var result4 = solution.FindDiagonalOrder(array4);
-        //
-        // int[][] array5 = { new[] { 6, 9, 7 } };
-        // var result5 = solution.FindDiagonalOrder(array5);
-        #endregion
+    #region Solution 57
+    //var result = solution.AddBinary("100", "110010");
+    #endregion
 
-        #region Solution 54
-        // int[][] array1 =
-        // {
-        //     new [] { 1, 2, 3 },
-        //     new [] { 4, 5, 6 },
-        //     new [] { 7, 8, 9 }
-        // };
-        // var result1 = solution.SpiralOrder(array1);
+    #region Solution 498
+    //  int[][] array1 =
+    //  {
+    //      new [] { 1, 2, 3 },
+    //      new [] { 4, 5, 6 },
+    //      new [] { 7, 8, 9 }
+    //  };
+    // var result1 = solution.FindDiagonalOrder(array1);
+    //
+    //  int[][] array2 =
+    //  {
+    //      new [] { 1, 2 },
+    //      new [] { 3, 4 }
+    //  };
+    // var result2 = solution.FindDiagonalOrder(array2);
+    //
+    // int[][] array3 = { new[] { 2, 3 } };
+    // var result3 = solution.FindDiagonalOrder(array3);
+    //
+    // int[][] array4 = { new[] { 3 } , new [] { 2 } };
+    // var result4 = solution.FindDiagonalOrder(array4);
+    //
+    // int[][] array5 = { new[] { 6, 9, 7 } };
+    // var result5 = solution.FindDiagonalOrder(array5);
+    #endregion
+
+    #region Solution 54
+    // int[][] array1 =
+    // {
+    //     new [] { 1, 2, 3 },
+    //     new [] { 4, 5, 6 },
+    //     new [] { 7, 8, 9 }
+    // };
+    // var result1 = solution.SpiralOrder(array1);
 
 
-        // // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
-        // int[][] array2 =
-        // {
-        //     new [] { 1, 2, 3, 4 },
-        //     new [] { 5, 6, 7, 8 },
-        //     new [] { 9, 10, 11, 12 },
-        //     new [] { 13, 14, 15, 16 },
-        //     new [] { 17, 18, 19, 20 },
-        //     new [] { 21, 22, 23, 24 }
-        // };
-        // // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
-        // var result2 = solution.SpiralOrder(array2);
-        #endregion
+    // // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
+    // int[][] array2 =
+    // {
+    //     new [] { 1, 2, 3, 4 },
+    //     new [] { 5, 6, 7, 8 },
+    //     new [] { 9, 10, 11, 12 },
+    //     new [] { 13, 14, 15, 16 },
+    //     new [] { 17, 18, 19, 20 },
+    //     new [] { 21, 22, 23, 24 }
+    // };
+    // // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
+    // var result2 = solution.SpiralOrder(array2);
+    #endregion
 
-        #region Solution 118
-        //var result = solution.Generate(5);
-        #endregion
-    //}
+    #region Solution 118
+    //var result = solution.Generate(5);
+    #endregion
 }
---
+++ CurrentTask/Solution_189.cs
@@ -0,0 +1,36 @@
+﻿// // 189. Rotate Array
+// namespace CurrentTask;
+//
+// public class Solution
+// {
+//     public void Rotate(int[] nums, int k)
+//     {
+//         var length = nums.Length;
+//         var normalizedK = k % length;
+//
+//         var rotatedNums = new int[length];
+//         var indexOriginal = length - normalizedK;
+//
+//         for (var indexRotated = 0; indexRotated < length; indexRotated++)
+//         {
+//             if (indexOriginal == length)
+//             {
+//                 indexOriginal = 0;
+//             }
+//
+//             if (indexRotated == length)
+//             {
+//                 indexRotated = 0;
+//             }
+//
+//             rotatedNums[indexRotated] = nums[indexOriginal];
+//
+//             indexOriginal++;
+//         }
+//
+//         for (var i = 0; i < length; i++)
+//         {
+//             nums[i] = rotatedNums[i];
+//         }
+//     }
+// }
