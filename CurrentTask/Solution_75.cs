eb2853224fcef20fa1ffb8adab72363a434cf941
Sergey Korepanov <igzhestik@mail.ru>
2025-01-09 21:55:28 +0500

    Task 75. Sort Colors

---
+++ CurrentTask/Solution_75.cs
@@ -0,0 +1,53 @@
+﻿// 75. Sort Colors
+namespace CurrentTask;
+
+public class Solution_75
+{
+    public void SortColors(int[] nums)
+    {
+        #region С помощью сортировки выбором
+        // for (var i = 0; i < nums.Length; i++)
+        // {
+        //     var smallestIndex = i;
+        //
+        //     for (var j = i + 1; j < nums.Length; j++)
+        //     {
+        //         if (nums[j] < nums[smallestIndex])
+        //         {
+        //             smallestIndex = j;
+        //         }
+        //     }
+        //
+        //     if (smallestIndex != i)
+        //     {
+        //         (nums[i], nums[smallestIndex]) = (nums[smallestIndex], nums[i]);
+        //     }
+        // }
+        #endregion
+
+        #region One-pass algorithm
+        var left = 0;
+        var right = nums.Length - 1;
+        var current = 0;
+
+        while (current <= right)
+        {
+            if (nums[current] == 0)
+            {
+                (nums[left], nums[current]) = (nums[current], nums[left]);
+                left++;
+                current++;
+            }
+            else if (nums[current] == 2)
+            {
+                (nums[right], nums[current]) = (nums[current], nums[right]);
+                right--;
+            }
+            else
+            {
+                current++;
+            }
+        }
+        #endregion
+    }
+}
