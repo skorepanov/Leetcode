6c1855076a2a0a19357c689f8ee572f5dc786b8c
Sergey Korepanov <igzhestik@mail.ru>
2025-02-18 16:44:19 +0500

    Task 1571. Warehouse Manager

---
+++ CurrentTask/Solution_1571.sql
@@ -0,0 +1,6 @@
+﻿select
+    w.name as warehouse_name,
+    sum(w.units * p.width * p.length * p.height) as volume
+from warehouse w
+    left join products p on w.product_id = p.product_id
+group by w.name
