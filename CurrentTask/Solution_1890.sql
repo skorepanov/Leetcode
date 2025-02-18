ca24bfb45f5ae7114d1753242a274b1520288186
Sergey Korepanov <igzhestik@mail.ru>
2025-02-18 16:27:44 +0500

    Task 1890. The Latest Login in 2020

---
+++ CurrentTask/Solution_1890.sql
@@ -0,0 +1,15 @@
+﻿-- 1890. The Latest Login in 2020
+-- my solution - faster
+select distinct on (user_id)
+    user_id,
+    time_stamp as last_stamp
+from logins
+where date_part('year', time_stamp) = 2020
+order by user_id, time_stamp desc
+
+-- with max() - slower
+select user_id,
+       max(time_stamp) as last_stamp
+from logins
+where date_part('year', time_stamp) = 2020
+group by user_id
