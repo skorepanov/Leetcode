07c4f70a817d8ca8413c35181f498beb0a0ec1dd
Sergey Korepanov <igzhestik@mail.ru>
2025-02-18 17:23:14 +0500

    Task 1173. Immediate Food Delivery I

---
+++ CurrentTask/Solution_1173.sql
@@ -0,0 +1,7 @@
+﻿-- 1173. Immediate Food Delivery I
+select
+    round(
+        (cast(count(*) filter (where order_date = customer_pref_delivery_date) as decimal(7,2)) / count(*)) * 100,
+        2
+    ) as immediate_percentage
+from delivery
