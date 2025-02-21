27f9572b8fdf21c84024774a596ed4754988dbef
Sergey Korepanov <igzhestik@mail.ru>
2025-02-21 22:00:33 +0500

    Task 1699. Number of Calls Between Two Persons

---
+++ CurrentTask/Solution_1699.sql
@@ -0,0 +1,62 @@
+﻿-- 1699. Number of Calls Between Two Persons
+
+-- 3rd way - runtime 850 ms, beats 33.67%
+select
+    least(from_id, to_id) as person1,
+    greatest(to_id, from_id) as person2,
+    count(*) as call_count,
+    sum(duration) as total_duration
+from calls
+group by person1, person2
+
+-- 2nd way - runtime 662 ms, beats 72.36%
+with filtered_calls as (
+    select
+        from_id as person1,
+        to_id as person2,
+        duration
+    from calls
+    where from_id < to_id
+        union all
+    select
+        to_id as person1,
+        from_id as person2,
+        duration
+    from calls
+    where to_id < from_id
+)
+select
+    person1,
+    person2,
+    count(*) as call_count,
+    sum(duration) as total_duration
+from filtered_calls
+group by person1, person2
+
+-- 1st way - runtime 1064 ms, beats 13.57%
+with grouped_calls as (
+    select
+        from_id as person1,
+        to_id as person2,
+        count(*) as call_count,
+        sum(duration) as total_duration
+    from calls
+    where from_id < to_id
+    group by from_id, to_id
+        union all
+    select
+        to_id as person1,
+        from_id as person2,
+        count(*) as call_count,
+        sum(duration) as total_duration
+    from calls
+    where to_id < from_id
+    group by to_id, from_id
+)
+select
+    person1,
+    person2,
+    sum(call_count) as call_count,
+    sum(total_duration) as total_duration
+from grouped_calls
+group by person1, person2
