e7e39948bd2876d5cd9808c8640e1d91a9bba365
Sergey Korepanov <igzhestik@mail.ru>
2025-02-21 21:40:36 +0500

    Task 1445. Apples & Oranges

---
+++ CurrentTask/Solution_1445.sql
@@ -0,0 +1,28 @@
+﻿-- 1445. Apples & Oranges
+
+-- 2 вариант - без CTE
+select
+    apples.sale_date,
+    sum(apples.sold_num - oranges.sold_num) as diff
+from sales apples
+         left join sales oranges on apples.sale_date = oranges.sale_date
+where apples.fruit = 'apples' and oranges.fruit = 'oranges'
+group by apples.sale_date
+order by apples.sale_date
+
+-- 1 вариант - с CTE
+/*
+with sales_by_date as (
+    select sale_date, fruit, sum(sold_num) as sold_num
+    from sales
+    group by sale_date, fruit
+)
+select
+    apples.sale_date,
+    apples.sold_num - oranges.sold_num as diff
+from sales_by_date apples
+    left join sales_by_date oranges
+        on apples.sale_date = oranges.sale_date and oranges.fruit = 'oranges'
+where apples.fruit = 'apples'
+order by apples.sale_date
+*/
