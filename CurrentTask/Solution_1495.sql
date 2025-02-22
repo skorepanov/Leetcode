fc54a510f9612bfc0cc2fc3e23c5f17821b54f01
Sergey Korepanov <igzhestik@mail.ru>
2025-02-22 19:21:32 +0500

    Task 1495. Friendly Movies Streamed Last Month

---
+++ CurrentTask/Solution_1495.sql
@@ -0,0 +1,9 @@
+﻿-- 1495. Friendly Movies Streamed Last Month
+select distinct
+    c.title
+from content c
+    inner join tvprogram p on c.content_id = p.content_id
+where c.kids_content = 'Y'
+    and c.content_type = 'Movies'
+    and date_part('year', p.program_date) = 2020
+    and date_part('month', p.program_date) = 6
