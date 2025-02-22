94f789d1844ca4eb2f94c1d467e3ec48fe74d253
Sergey Korepanov <igzhestik@mail.ru>
2025-02-22 18:50:52 +0500

    Task 1587. Bank Account Summary II

---
+++ CurrentTask/Solution_1587.sql
@@ -0,0 +1,8 @@
+﻿-- 1587. Bank Account Summary II
+select
+    u.name,
+    sum(t.amount) as balance
+from transactions t
+    inner join users u on t.account = u.account
+group by u.name
+having sum(t.amount) > 10000
