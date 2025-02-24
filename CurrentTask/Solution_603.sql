27079a0c9da09b743cde67e2a9f24e7abc42d93c
Sergey Korepanov <igzhestik@mail.ru>
2025-02-24 17:00:32 +0500

    Task 603. Consecutive Available Seats

---
+++ CurrentTask/Solution_603.sql
@@ -0,0 +1,19 @@
+﻿-- 603. Consecutive Available Seats
+
+-- 2nd way - runtime 493 ms, beats 19.86%
+select distinct
+    c1.seat_id
+from Cinema c1
+    inner join Cinema c2 on c1.seat_id in (c2.seat_id - 1, c2.seat_id + 1)
+where c1.free = 1 and c2.free = 1
+order by c1.seat_id
+
+-- 1st way - runtime 344 ms, beats 55.60%
+select
+    c1.seat_id
+from Cinema c1
+    left join Cinema c2 on c1.seat_id = c2.seat_id - 1
+    left join Cinema c3 on c1.seat_id = c3.seat_id + 1
+where c1.free = 1
+    and (c2.free = 1 or c3.free = 1)
+order by c1.seat_id
