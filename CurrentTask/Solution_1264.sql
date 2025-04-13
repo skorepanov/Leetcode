f2eb48258b08eeaf740e7decb0971fb2c9e8427b
Sergey Korepanov <igzhestik@mail.ru>
2025-04-13 15:00:36 +0500

    Task 1264. Page Recommendations

---
+++ CurrentTask/Solution_1264.sql
@@ -0,0 +1,10 @@
+﻿-- 1264. Page Recommendations
+select distinct
+    l.page_id as recommended_page
+from Friendship f
+    inner join Likes l on l.user_id in (f.user1_id, f.user2_id)
+where f.user1_id = 1 or f.user2_id = 1
+    except
+select page_id
+from Likes
+where user_id = 1
