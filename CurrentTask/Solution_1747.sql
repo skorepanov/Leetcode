9e29693b46a737e35e69026e51357baf65bdf8b1
Sergey Korepanov <igzhestik@mail.ru>
2025-04-20 21:47:30 +0500

    Task 1747. Leetflex Banned Accounts

---
+++ CurrentTask/Solution_1747.sql
@@ -0,0 +1,8 @@
+﻿-- 1747. Leetflex Banned Accounts
+select distinct
+    li1.account_id
+from LogInfo li1
+    inner join LogInfo li2
+        on li1.account_id = li2.account_id
+            and li1.ip_address <> li2.ip_address
+            and li1.login between li2.login and li2.logout
