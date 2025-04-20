1c8c7b777c3e439080c87ddf3757080ac8ae432c
Sergey Korepanov <igzhestik@mail.ru>
2025-04-20 21:05:10 +0500

    Task 608. Tree Node

---
+++ CurrentTask/Solution_608.sql
@@ -0,0 +1,10 @@
+﻿-- 608. Tree Node
+select distinct
+    parent.id,
+    case
+        when parent.p_id is null then 'Root'
+        when child.id is null then 'Leaf'
+        else 'Inner'
+    end as type
+from Tree parent
+    left join Tree child on parent.id = child.p_id
