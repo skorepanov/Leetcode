1296e78f53d905e21cce07d2852dddd4a7a03259
Sergey Korepanov <igzhestik@mail.ru>
2025-04-21 21:36:32 +0500

    Task 1709. Biggest Window Between Visits

---
+++ CurrentTask/Solution_1709.sql
@@ -0,0 +1,19 @@
+﻿-- 1709. Biggest Window Between Visits
+with visits as (
+    select
+        uv1.user_id,
+        uv1.visit_date,
+        (
+            select
+                coalesce(min(visit_date), '20210101')
+            from UserVisits uv2
+            where uv2.user_id = uv1.user_id
+                and uv2.visit_date > uv1.visit_date
+        ) as next_visit_date
+    from UserVisits uv1
+)
+select
+    user_id,
+    max(next_visit_date - visit_date) as biggest_window
+from visits
+group by user_id
