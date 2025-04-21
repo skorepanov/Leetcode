3152640cbc2eac1a15b5ae0f0a5b04063d8aaf2b
Sergey Korepanov <igzhestik@mail.ru>
2025-04-21 17:47:05 +0500

    Task 1532. The Most Recent Three Orders

---
+++ CurrentTask/Solution_1532.sql
@@ -0,0 +1,19 @@
+﻿-- 1532. The Most Recent Three Orders
+with recent_orders as (
+    select
+        order_id,
+        row_number() over (partition by customer_id order by order_date desc) as row_number
+    from Orders
+)
+select
+    c.name as customer_name,
+    c.customer_id,
+    o.order_id,
+    o.order_date
+from recent_orders ro
+    inner join Orders o
+        on ro.order_id = o.order_id
+    inner join Customers c
+        on o.customer_id = c.customer_id
+where ro.row_number <= 3
+order by c.name, c.customer_id, o.order_date desc
