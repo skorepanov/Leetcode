59a27b2112619817c109cde30711574776106fdc
Sergey Korepanov <igzhestik@mail.ru>
2025-04-21 19:49:23 +0500

    Task 1831. Maximum Transaction Each Day

---
+++ CurrentTask/Solution_1831.sql
@@ -0,0 +1,13 @@
+﻿-- 1831. Maximum Transaction Each Day
+with ranked_transactions as (
+    select
+        transaction_id,
+        rank() over (partition by day order by amount desc) as rank
+    from Transactions
+    order by day, amount desc
+)
+select
+    transaction_id
+from ranked_transactions
+where rank = 1
+order by transaction_id
