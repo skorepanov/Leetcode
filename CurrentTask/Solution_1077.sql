75af4901db982870dc58671f10408c8de3664a63
Sergey Korepanov <igzhestik@mail.ru>
2025-04-21 20:01:50 +0500

    Task 1077. Project Employees III

---
+++ CurrentTask/Solution_1077.sql
@@ -0,0 +1,15 @@
+﻿-- 1077. Project Employees III
+with ranked_employees as (
+    select
+        p.project_id,
+        p.employee_id,
+        rank() over (partition by p.project_id order by e.experience_years desc) as rank
+    from Project p
+        inner join Employee e
+            on p.employee_id = e.employee_id
+)
+select
+    project_id,
+    employee_id
+from ranked_employees
+where rank = 1
