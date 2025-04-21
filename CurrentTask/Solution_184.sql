b71695b8c6f937019b02b6f55d4319393eba4da5
Sergey Korepanov <igzhestik@mail.ru>
2025-04-21 17:24:39 +0500

    Task 184. Department Highest Salary

---
+++ CurrentTask/Solution_184.sql
@@ -0,0 +1,17 @@
+﻿-- 184. Department Highest Salary
+with max_salaries as (
+    select
+        departmentId,
+        max(salary) as salary
+    from Employee
+    group by departmentId
+)
+select
+    d.name as department,
+    e.name as employee,
+    ms.salary
+from max_salaries ms
+    inner join Employee e
+        on ms.departmentId = e.departmentId and ms.salary = e.salary
+    inner join Department d
+        on ms.departmentId = d.id
