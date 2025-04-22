78935911d1dfcbfa0b5fa0957bf80abf29a32446
Sergey Korepanov <igzhestik@mail.ru>
2025-04-22 16:48:08 +0500

    Task 1767. Find the Subtasks That Did Not Execute

---
+++ CurrentTask/Solution_1767.sql
@@ -0,0 +1,20 @@
+﻿-- 1767. Find the Subtasks That Did Not Execute
+with recursive subtask_ids as (
+    select 1 as id
+        union all
+    select
+        id + 1
+    from subtask_ids
+    where id + 1 <= 20
+)
+select
+    t.task_id,
+    si.id as subtask_id
+from subtask_ids si
+    inner join Tasks t
+        on si.id <= t.subtasks_count
+    except
+select
+    task_id,
+    subtask_id
+from Executed
