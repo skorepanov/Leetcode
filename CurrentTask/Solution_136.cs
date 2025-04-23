dfbcb66b0879d21fe9a60679efbf3948c2036dfe
Sergey Korepanov <igzhestik@mail.ru>
2025-04-23 18:59:49 +0500

    Task 136. Single Number

---
+++ CurrentTask/Solution_136.cs
@@ -0,0 +1,49 @@
+﻿// 136. Single Number
+namespace CurrentTask;
+
+#region 2 вариант - с помощью XOR
+public class Solution_136_2
+{
+    /*
+    a⊕0=a
+    a⊕a=0
+    a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+    */
+
+    public int SingleNumber(int[] nums)
+    {
+        var a = 0;
+
+        foreach (var num in nums)
+        {
+            a ^= num;
+        }
+
+        return a;
+    }
+}
+#endregion
+
+#region 1 вариант - с помощью HashSet
+public class Solution_136_1
+{
+    public int SingleNumber(int[] nums)
+    {
+        var hashset = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (hashset.Contains(num))
+            {
+                hashset.Remove(num);
+            }
+            else
+            {
+                hashset.Add(num);
+            }
+        }
+
+        return hashset.First();
+    }
+}
+#endregion
