b5a312ac3c6df6fafda7b83ee9263cd7f5a93b6b
Sergey Korepanov <igzhestik@mail.ru>
2025-04-23 19:56:11 +0500

    .NET 6 -> .NET 9

--- CurrentTask/Program.cs
+++ CurrentTask/Program.cs
@@ -1,168 +1,160 @@
-﻿using CurrentTask;
+﻿var solution = new Solution();
 
-public class Solution1
-{
-    public static void Main()
-    {
-        var solution = new Solution();
-    }
+#region Solution 706
+// var myHashMap = new MyHashMap();
+// myHashMap.Put(1, 1);
+// var result1 = myHashMap.Get(1);
+#endregion
 
-    #region Solution 706
-    // var myHashMap = new MyHashMap();
-    // myHashMap.Put(1, 1);
-    // var result1 = myHashMap.Get(1);
-    #endregion
+#region Solution 705
+// var myHashSet = new MyHashSet();
+// myHashSet.Add(1);
+// myHashSet.Add(2);
+// var result1 = myHashSet.Contains(1);
+// var result2 = myHashSet.Contains(3);
+// myHashSet.Add(2);
+// var result3 = myHashSet.Contains(2);
+// myHashSet.Remove(2);
+// var result4 = myHashSet.Contains(2);
+//
+// myHashSet.Add(0);
+// var result5 = myHashSet.Contains(0);
+// myHashSet.Add(1000000);
+// var result6 = myHashSet.Contains(1000000);
+// myHashSet.Add(33003);
+// var result7 = myHashSet.Contains(33003);
+#endregion
 
-    #region Solution 705
-    // var myHashSet = new MyHashSet();
-    // myHashSet.Add(1);
-    // myHashSet.Add(2);
-    // var result1 = myHashSet.Contains(1);
-    // var result2 = myHashSet.Contains(3);
-    // myHashSet.Add(2);
-    // var result3 = myHashSet.Contains(2);
-    // myHashSet.Remove(2);
-    // var result4 = myHashSet.Contains(2);
-    //
-    // myHashSet.Add(0);
-    // var result5 = myHashSet.Contains(0);
-    // myHashSet.Add(1000000);
-    // var result6 = myHashSet.Contains(1000000);
-    // myHashSet.Add(33003);
-    // var result7 = myHashSet.Contains(33003);
-    #endregion
+#region Solution 347
+// var nums = new[] { 4, 1, -1, 2, -1, 2, 3 };
+// var result = solution.TopKFrequent(nums, k: 2);
+#endregion
 
-    #region Solution 347
-    // var nums = new[] { 4, 1, -1, 2, -1, 2, 3 };
-    // var result = solution.TopKFrequent(nums, k: 2);
-    #endregion
+#region Solution 2343
+// var nums = new[] { "102", "473", "251", "814" };
+// var queries = new[]
+// {
+//     new int[] { 1, 1 },
+//     new int[] { 2, 3 },
+//     new int[] { 4, 2 },
+//     new int[] { 1, 2 }
+// };
+// var result = solution.SmallestTrimmedNumbers(nums, queries);
+#endregion
 
-    #region Solution 2343
-    // var nums = new[] { "102", "473", "251", "814" };
-    // var queries = new[]
-    // {
-    //     new int[] { 1, 1 },
-    //     new int[] { 2, 3 },
-    //     new int[] { 4, 2 },
-    //     new int[] { 1, 2 }
-    // };
-    // var result = solution.SmallestTrimmedNumbers(nums, queries);
-    #endregion
+#region Solution 1200
+// var arr = new[] { -25, -51, -29, -23, 57, -18 };
+// var result = solution.MinimumAbsDifference(arr);
+#endregion
 
-    #region Solution 1200
-    // var arr = new[] { -25, -51, -29, -23, 57, -18 };
-    // var result = solution.MinimumAbsDifference(arr);
-    #endregion
+#region Solution 215
+// var nums = new [] { 3, 2, 1, 5, 6, 4 };
+// var result = solution.FindKthLargest(nums, 2);
+#endregion
 
-    #region Solution 215
-    // var nums = new [] { 3, 2, 1, 5, 6, 4 };
-    // var result = solution.FindKthLargest(nums, 2);
-    #endregion
+#region Solution 912
+// var nums = new [] { 20, 10, 15, 5, 1, 3, 2 };
+// var result = solution.SortArray(nums);
+#endregion
 
-    #region Solution 912
-    // var nums = new [] { 20, 10, 15, 5, 1, 3, 2 };
-    // var result = solution.SortArray(nums);
-    #endregion
+#region Solution 147
+// var listNode3 = new ListNode(3);
+// var listNode2 = new ListNode(1, listNode3);
+// var listNode1 = new ListNode(2, listNode2);
+// var listNode0 = new ListNode(4, listNode1);
+// var result = solution.InsertionSortList(listNode0);
+#endregion
 
-    #region Solution 147
-    // var listNode3 = new ListNode(3);
-    // var listNode2 = new ListNode(1, listNode3);
-    // var listNode1 = new ListNode(2, listNode2);
-    // var listNode0 = new ListNode(4, listNode1);
-    // var result = solution.InsertionSortList(listNode0);
-    #endregion
+#region Solution 557
+//var result = solution.ReverseWords("Let's take LeetCode contest");
+#endregion
 
-    #region Solution 557
-    //var result = solution.ReverseWords("Let's take LeetCode contest");
-    #endregion
+#region Solution 151
+//var result = solution.ReverseWords("  hello      world  ");
+#endregion
 
-    #region Solution 151
-    //var result = solution.ReverseWords("  hello      world  ");
-    #endregion
+#region Solution 119
+//var result = solution.GetRow(5);
+#endregion
 
-    #region Solution 119
-    //var result = solution.GetRow(5);
-    #endregion
+#region Solution 189
+// var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+// solution.Rotate(nums, 1);
+#endregion
 
-    #region Solution 189
-    // var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
-    // solution.Rotate(nums, 1);
-    #endregion
+#region Solution 209
+//var result = solution.MinSubArrayLen(7, new[] { 2, 3, 1, 2, 4, 3 });
+#endregion
 
-    #region Solution 209
-    //var result = solution.MinSubArrayLen(7, new[] { 2, 3, 1, 2, 4, 3 });
-    #endregion
+#region Solution 561
+// //var nums = new int[] { 1, 4, 3, 2 };
+// var nums = new int[] { 6, 2, 6, 5, 1, 2 };
+// var result = solution.ArrayPairSum(nums);
+#endregion
 
-    #region Solution 561
-    // //var nums = new int[] { 1, 4, 3, 2 };
-    // var nums = new int[] { 6, 2, 6, 5, 1, 2 };
-    // var result = solution.ArrayPairSum(nums);
-    #endregion
+#region Solution 344
+// var s = new [] { '1', '2', '3', '4', '5' };
+// solution.ReverseString(s);
+#endregion
 
-    #region Solution 344
-    // var s = new [] { '1', '2', '3', '4', '5' };
-    // solution.ReverseString(s);
-    #endregion
+#region Solution 14
+//var result = solution.LongestCommonPrefix(new [] { "flower","flow","flight" });
+#endregion
 
-    #region Solution 14
-    //var result = solution.LongestCommonPrefix(new [] { "flower","flow","flight" });
-    #endregion
+#region Solution 57
+//var result = solution.AddBinary("100", "110010");
+#endregion
 
-    #region Solution 57
-    //var result = solution.AddBinary("100", "110010");
-    #endregion
+#region Solution 498
+//  int[][] array1 =
+//  {
+//      new [] { 1, 2, 3 },
+//      new [] { 4, 5, 6 },
+//      new [] { 7, 8, 9 }
+//  };
+// var result1 = solution.FindDiagonalOrder(array1);
+//
+//  int[][] array2 =
+//  {
+//      new [] { 1, 2 },
+//      new [] { 3, 4 }
+//  };
+// var result2 = solution.FindDiagonalOrder(array2);
+//
+// int[][] array3 = { new[] { 2, 3 } };
+// var result3 = solution.FindDiagonalOrder(array3);
+//
+// int[][] array4 = { new[] { 3 } , new [] { 2 } };
+// var result4 = solution.FindDiagonalOrder(array4);
+//
+// int[][] array5 = { new[] { 6, 9, 7 } };
+// var result5 = solution.FindDiagonalOrder(array5);
+#endregion
 
-    #region Solution 498
-    //  int[][] array1 =
-    //  {
-    //      new [] { 1, 2, 3 },
-    //      new [] { 4, 5, 6 },
-    //      new [] { 7, 8, 9 }
-    //  };
-    // var result1 = solution.FindDiagonalOrder(array1);
-    //
-    //  int[][] array2 =
-    //  {
-    //      new [] { 1, 2 },
-    //      new [] { 3, 4 }
-    //  };
-    // var result2 = solution.FindDiagonalOrder(array2);
-    //
-    // int[][] array3 = { new[] { 2, 3 } };
-    // var result3 = solution.FindDiagonalOrder(array3);
-    //
-    // int[][] array4 = { new[] { 3 } , new [] { 2 } };
-    // var result4 = solution.FindDiagonalOrder(array4);
-    //
-    // int[][] array5 = { new[] { 6, 9, 7 } };
-    // var result5 = solution.FindDiagonalOrder(array5);
-    #endregion
-
-    #region Solution 54
-    // int[][] array1 =
-    // {
-    //     new [] { 1, 2, 3 },
-    //     new [] { 4, 5, 6 },
-    //     new [] { 7, 8, 9 }
-    // };
-    // var result1 = solution.SpiralOrder(array1);
+#region Solution 54
+// int[][] array1 =
+// {
+//     new [] { 1, 2, 3 },
+//     new [] { 4, 5, 6 },
+//     new [] { 7, 8, 9 }
+// };
+// var result1 = solution.SpiralOrder(array1);
 
 
-    // // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
-    // int[][] array2 =
-    // {
-    //     new [] { 1, 2, 3, 4 },
-    //     new [] { 5, 6, 7, 8 },
-    //     new [] { 9, 10, 11, 12 },
-    //     new [] { 13, 14, 15, 16 },
-    //     new [] { 17, 18, 19, 20 },
-    //     new [] { 21, 22, 23, 24 }
-    // };
-    // // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
-    // var result2 = solution.SpiralOrder(array2);
-    #endregion
+// // [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16], [17,18,19,20], [21,22,23,24]]
+// int[][] array2 =
+// {
+//     new [] { 1, 2, 3, 4 },
+//     new [] { 5, 6, 7, 8 },
+//     new [] { 9, 10, 11, 12 },
+//     new [] { 13, 14, 15, 16 },
+//     new [] { 17, 18, 19, 20 },
+//     new [] { 21, 22, 23, 24 }
+// };
+// // required output: [1,2,3,4,8,12,16,20,24,23,22,21,17,13,9,5,6,7,11,15,19,18,14,10]
+// var result2 = solution.SpiralOrder(array2);
+#endregion
 
-    #region Solution 118
-    //var result = solution.Generate(5);
-    #endregion
-}
+#region Solution 118
+//var result = solution.Generate(5);
+#endregion
