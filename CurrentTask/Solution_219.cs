65fe37319a7b95c8fce43667472a11fbbe59ffc8
Sergey Korepanov <igzhestik@mail.ru>
2025-04-27 18:41:37 +0500

    Solution 219. Contains Duplicate II

---
+++ CurrentTask/Solution_219.cs
@@ -0,0 +1,30 @@
+﻿// 219. Contains Duplicate II
+namespace CurrentTask;
+
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (!dict.ContainsKey(num))
+            {
+                dict.Add(num, i);
+                continue;
+            }
+
+            if (i - dict[num] <= k)
+            {
+                return true;
+            }
+
+            dict[num] = i;
+        }
+
+        return false;
+    }
+}
