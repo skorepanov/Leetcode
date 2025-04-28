c18d84b3edfc1486840f68e2e734821676b56010
Sergey Korepanov <igzhestik@mail.ru>
2025-04-28 20:33:40 +0500

    Solution 249. Group Shifted Strings

---
+++ CurrentTask/Solution_249.cs
@@ -0,0 +1,46 @@
+﻿// 249. Group Shifted Strings
+namespace CurrentTask;
+
+public class Solution_249
+{
+    public IList<IList<string>> GroupStrings(string[] strings)
+    {
+        var groupedStrings = new Dictionary<string, List<string>>();
+
+        foreach (var str in strings)
+        {
+            var baseString = ToBaseString(str);
+
+            if (!groupedStrings.ContainsKey(baseString))
+            {
+                groupedStrings.Add(baseString, []);
+            }
+
+            groupedStrings[baseString].Add(str);
+        }
+
+        return new List<IList<string>>(groupedStrings.Values);
+    }
+
+    private string ToBaseString(string str)
+    {
+        var baseString = str.ToCharArray();
+
+        while (baseString[0] != 'a')
+        {
+            for (var i = 0; i < baseString.Length; i++)
+            {
+                if (baseString[i] == 'z')
+                {
+                    baseString[i] = 'a';
+                }
+                else
+                {
+                    baseString[i]++;
+                }
+            }
+        }
+
+        return new string(baseString);
+    }
+}
