c7bab97796396b0e4b244911d76ff1e39581ba55
Sergey Korepanov <igzhestik@mail.ru>
2025-05-01 13:28:32 +0500

    Solution 771. Jewels and Stones

---
+++ CurrentTask/Solution_771.cs
@@ -0,0 +1,44 @@
+﻿// 771. Jewels and Stones
+namespace CurrentTask;
+
+#region Вариант 1 - Runtime 0 ms, Beats 100.00%
+public class Solution_771_1
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var jewelCount = 0;
+
+        foreach (var stone in stones)
+        {
+            if (jewels.Contains(stone))
+            {
+                jewelCount++;
+            }
+        }
+
+        return jewelCount;
+    }
+}
+#endregion
+
+#region Вариант 2 - Runtime 0 ms, Beats 100.00%
+public class Solution_771_2
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var jewelCount = 0;
+
+        var jewelsAsHashSet = new HashSet<char>(jewels);
+
+        foreach (var stone in stones)
+        {
+            if (jewelsAsHashSet.Contains(stone))
+            {
+                jewelCount++;
+            }
+        }
+
+        return jewelCount;
+    }
+}
+#endregion
