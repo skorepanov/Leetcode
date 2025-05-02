100902a39b3217bc5eda4287cc0c60d737bd8b3e
Sergey Korepanov <igzhestik@mail.ru>
2025-05-02 13:49:25 +0500

    Solution 170. Two Sum III - Data structure design

---
+++ CurrentTask/Solution_170.cs
@@ -0,0 +1,51 @@
+﻿// 170. Two Sum III - Data structure design
+namespace CurrentTask;
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.Add(number);
+ * bool param_2 = obj.Find(value);
+ */
+
+public class TwoSum_170
+{
+    private readonly Dictionary<int, int> _numberToCountPairs;
+
+    public TwoSum_170()
+    {
+        _numberToCountPairs = new Dictionary<int, int>();
+    }
+
+    public void Add(int number)
+    {
+        if (!_numberToCountPairs.ContainsKey(number))
+        {
+            _numberToCountPairs.Add(number, 0);
+        }
+
+        _numberToCountPairs[number]++;
+    }
+
+    public bool Find(int value)
+    {
+        foreach (var numberToCountPair in _numberToCountPairs)
+        {
+            var complement = value - numberToCountPair.Key;
+
+            if (numberToCountPair.Key == complement)
+            {
+                if (numberToCountPair.Value >= 2)
+                {
+                    return true;
+                }
+            }
+            else if (_numberToCountPairs.ContainsKey(complement))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
