7342735b17de947c90d6bc323a87dc49fcc47651
Sergey Korepanov <igzhestik@mail.ru>
2025-05-29 17:23:01 +0500

    Solution 20. Valid Parentheses

---
+++ Leetcode/Solution_20.cs
@@ -0,0 +1,33 @@
+﻿// 20. Valid Parentheses
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var inputBrackets = new Stack<char>();
+
+        var closeToOpenBrackets = new Dictionary<char, char>()
+        {
+             [')'] = '(',
+             ['}'] = '{',
+             [']'] = '['
+        };
+
+        foreach (var bracket in s)
+        {
+            if (closeToOpenBrackets.ContainsValue(bracket))
+            {
+                inputBrackets.Push(bracket);
+                continue;
+            }
+
+            var top = inputBrackets.Count == 0 ? '#' : inputBrackets.Pop();
+
+            if (top != closeToOpenBrackets[bracket])
+            {
+                return false;
+            }
+        }
+
+        return inputBrackets.Count == 0;
+    }
+}
