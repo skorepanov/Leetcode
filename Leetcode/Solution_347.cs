7b889e532798fa2d7c6d7d68be893098eb7fcff1
Sergey Korepanov <igzhestik@mail.ru>
2025-06-18 20:12:26 +0500

    Solution 347. Top K Frequent Elements. Реализовано решение с помощью Dictionary и Heap

--- Leetcode/Solution_347.cs
+++ Leetcode/Solution_347.cs
@@ -1,5 +1,7 @@
 ﻿// 347. Top K Frequent Elements
-public class Solution_347
+
+#region Solution 1 - С помощью Dictionary - Runtime 11 ms, Beats 34.99%
+public class Solution_347_1
 {
     public int[] TopKFrequent(int[] nums, int k)
     {
@@ -26,3 +28,43 @@ public class Solution_347
         return topKFrequent;
     }
 }
+#endregion
+
+#region Solution 2 - С помощью Dictionary и Heap - Runtime 7 ms, Beats 75.52%
+public class Solution_347_2
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var frequencies = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (frequencies.ContainsKey(num))
+            {
+                frequencies[num]++;
+            }
+            else
+            {
+                frequencies.Add(num, 1);
+            }
+        }
+
+        var comparer = Comparer<int>.Create((x, y) => y.CompareTo(x));
+        var heap = new PriorityQueue<int, int>(comparer);
+
+        foreach (var frequency in frequencies)
+        {
+            heap.Enqueue(frequency.Key, frequency.Value);
+        }
+
+        var topKFrequent = new int[k];
+
+        for (var i = 0; i < k; i++)
+        {
+            topKFrequent[i] = heap.Dequeue();
+        }
+
+        return topKFrequent;
+    }
+}
+#endregion
