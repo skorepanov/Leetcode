6b4c79652818d058177eb0f899f609a42b578564
Sergey Korepanov <igzhestik@mail.ru>
2025-06-20 16:57:31 +0500

    Solution 1046. Last Stone Weight

---
+++ Leetcode/Solution_1046.cs
@@ -0,0 +1,30 @@
+﻿// 1046. Last Stone Weight
+public class Solution
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        var comparer = Comparer<int>.Create((x, y) => y.CompareTo(x));
+        var orderedStones = new PriorityQueue<int, int>(comparer);
+
+        foreach (var stone in stones)
+        {
+            orderedStones.Enqueue(stone, stone);
+        }
+
+        while (orderedStones.Count > 1)
+        {
+            var x = orderedStones.Dequeue();
+            var y = orderedStones.Dequeue();
+
+            if (x != y)
+            {
+                var diff = x - y;
+                orderedStones.Enqueue(diff, diff);
+            }
+        }
+
+        return orderedStones.Count > 0
+            ? orderedStones.Dequeue()
+            : 0;
+    }
+}
