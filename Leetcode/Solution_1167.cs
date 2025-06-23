be61fbc81de3966d2d6392eb7fda539c16b553e9
Sergey Korepanov <igzhestik@mail.ru>
2025-06-23 21:47:27 +0500

    Solution 1167. Minimum Cost to Connect Sticks

---
+++ Leetcode/Solution_1167.cs
@@ -0,0 +1,28 @@
+﻿// 1167. Minimum Cost to Connect Sticks
+public class Solution_1167
+{
+    public int ConnectSticks(int[] sticks)
+    {
+        var orderedSticks = new PriorityQueue<int, int>();
+
+        foreach (var stick in sticks)
+        {
+            orderedSticks.Enqueue(stick, stick);
+        }
+
+        var cost = 0;
+
+        while (orderedSticks.Count > 1)
+        {
+            var stick1 = orderedSticks.Dequeue();
+            var stick2 = orderedSticks.Dequeue();
+
+            var diff = stick1 + stick2;
+            cost += diff;
+
+            orderedSticks.Enqueue(diff, diff);
+        }
+
+        return cost;
+    }
+}
