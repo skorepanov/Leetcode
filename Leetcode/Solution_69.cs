01dcad5043deaeb3143e198faacbf6b19ddcb45a
Sergey Korepanov <igzhestik@mail.ru>
2025-07-16 15:57:13 +0500

    Solution 69. Sqrt(x)

---
+++ Leetcode/Solution_69.cs
@@ -0,0 +1,36 @@
+﻿// 69. Sqrt(x)
+public class Solution_69
+{
+    public int MySqrt(int x)
+    {
+        if (x < 2)
+        {
+            return x;
+        }
+
+        var left = 1;
+        var right = x / 2;
+
+        while (left <= right)
+        {
+            var middle = left + (right - left) / 2;
+
+            if (middle <= x / middle
+                && x / (middle + 1) < middle + 1)
+            {
+                return middle;
+            }
+
+            if (middle < x / middle)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
+
+        return 0;
+    }
+}
