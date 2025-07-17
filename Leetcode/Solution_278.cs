8bfce41b33eaba7251478b87d0905aff9dc082a1
Sergey Korepanov <igzhestik@mail.ru>
2025-07-17 13:52:53 +0500

    Solution 278. First Bad Version

---
+++ Leetcode/Solution_278.cs
@@ -0,0 +1,40 @@
+﻿// 278. First Bad Version
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version);
+*/
+
+public class Solution_278 : VersionControl_278
+{
+    public int FirstBadVersion(int n)
+    {
+        var left = 0;
+        var right = n;
+
+        while (left < right)
+        {
+            var middle = left + (right - left) / 2;
+            var isBad = IsBadVersion(middle);
+
+            if (isBad)
+            {
+                right = middle;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
+
+// !!! мок
+public class VersionControl_278
+{
+    public bool IsBadVersion(int version)
+    {
+        return default;
+    }
+}
