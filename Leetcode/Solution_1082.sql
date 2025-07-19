a84507baafd02d34d16060613949f11c0130e001
Sergey Korepanov <igzhestik@mail.ru>
2025-07-19 22:00:41 +0500

    Solution 1082. Sales Analysis I

---
+++ Leetcode/Solution_1082.sql
@@ -0,0 +1,11 @@
+﻿-- 1082. Sales Analysis I
+with max_total_sum as (
+    select
+        seller_id,
+        sum(s.price) as total_price
+    from Sales s
+    group by s.seller_id
+)
+select seller_id
+from max_total_sum
+where total_price = (select max(total_price) from max_total_sum)
