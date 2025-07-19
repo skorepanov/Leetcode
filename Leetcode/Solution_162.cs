e7088cc3a64394fab6b9a221544c9db19d87be21
Sergey Korepanov <igzhestik@mail.ru>
2025-07-19 23:08:53 +0500

    Solution 162. Find Peak Element. Упрощён код

--- Leetcode/Solution_162.cs
+++ Leetcode/Solution_162.cs
@@ -10,17 +10,7 @@ public class Solution_162
         {
             var middle = left + (right - left) / 2;
 
-            if (middle == 0)
-            {
-                return nums[0] > nums[1] ? 0 : 1;
-            }
-
-            if (nums[middle - 1] < nums[middle] && nums[middle] > nums[middle + 1])
-            {
-                return middle;
-            }
-
-            if (nums[middle - 1] > nums[middle])
+            if (nums[middle] > nums[middle + 1])
             {
                 right = middle;
             }
