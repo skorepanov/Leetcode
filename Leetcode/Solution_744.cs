3bc0e06128d3f428e1c1adef47a2c70556841e5b
Sergey Korepanov <igzhestik@mail.ru>
2025-07-22 15:18:34 +0500

    Solution 744. Find Smallest Letter Greater Than Target

---
+++ Leetcode/Solution_744.cs
@@ -0,0 +1,27 @@
+﻿// 744. Find Smallest Letter Greater Than Target
+public class Solution_744
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        var left = 0;
+        var right = letters.Length - 1;
+
+        while (left < right)
+        {
+            var middle = left + (right - left) / 2;
+
+            if (letters[middle] <= target)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+
+        return letters[left] > target
+            ? letters[left]
+            : letters[0];
+    }
+}
