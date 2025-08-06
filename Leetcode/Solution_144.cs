4092436ef3257bb5ab6309a1960bb6ad0b3ad96f
Sergey Korepanov <igzhestik@mail.ru>
2025-08-06 15:52:49 +0500

    Solution 144. Binary Tree Preorder Traversal

---
+++ Leetcode/Solution_144.cs
@@ -0,0 +1,38 @@
+﻿public class Solution_144
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        var traversal = new List<int>();
+
+        Traverse(root, traversal);
+
+        return traversal;
+    }
+
+    private void Traverse(TreeNode node, List<int> traversal)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        traversal.Add(node.val);
+
+        Traverse(node.left, traversal);
+        Traverse(node.right, traversal);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
