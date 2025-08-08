a2c5a2fa3ca7e667564d0a6ae1c4a3e786aa8b1a
Sergey Korepanov <igzhestik@mail.ru>
2025-08-08 17:11:17 +0500

    Solution 145. Binary Tree Postorder Traversal. Добавлена итеративная реализация

--- Leetcode/Solution_145.cs
+++ Leetcode/Solution_145.cs
@@ -1,5 +1,7 @@
 ﻿// 145. Binary Tree Postorder Traversal
-public class Solution_145
+
+#region Вариант 1 - Рекурсивный
+public class Solution_145_1
 {
     public IList<int> PostorderTraversal(TreeNode root)
     {
@@ -36,3 +38,54 @@ public class Solution_145
         }
     }
 }
+#endregion
+
+#region Вариант 2 - Итеративный
+public class Solution_145_2
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        if (root is null)
+        {
+            return new List<int>();
+        }
+
+        var traversal = new List<int>();
+        var pendingNodes = new Stack<TreeNode>();
+        var currentNode = root;
+
+        while (currentNode is not null || pendingNodes.Count > 0)
+        {
+            if (currentNode is not null)
+            {
+                traversal.Add(currentNode.val);
+                pendingNodes.Push(currentNode);
+                currentNode = currentNode.right;
+            }
+            else
+            {
+                currentNode = pendingNodes.Pop();
+                currentNode = currentNode.left;
+            }
+        }
+
+        traversal.Reverse();
+
+        return traversal;
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+#endregion
