739a5aa1d4d240e87cb94ac7290e2b8fe49fb64c
Sergey Korepanov <igzhestik@mail.ru>
2025-08-08 17:47:20 +0500

    Solution 102. Binary Tree Level Order Traversal

---
+++ Leetcode/Solution_102.cs
@@ -0,0 +1,59 @@
+﻿// 102. Binary Tree Level Order Traversal
+public class Solution_102
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        if (root is null)
+        {
+            return [];
+        }
+
+        var traversal = new List<IList<int>>();
+        var pendingNodes = new Queue<TreeNode>();
+
+        pendingNodes.Enqueue(root);
+
+        while (pendingNodes.Count > 0)
+        {
+            var queueLength = pendingNodes.Count;
+            var currentLevelValues = new List<int>();
+            TreeNode currentNode;
+
+            while (queueLength > 0)
+            {
+                currentNode = pendingNodes.Dequeue();
+                currentLevelValues.Add(currentNode.val);
+
+                if (currentNode.left is not null)
+                {
+                    pendingNodes.Enqueue(currentNode.left);
+                }
+
+                if (currentNode.right is not null)
+                {
+                    pendingNodes.Enqueue(currentNode.right);
+                }
+
+                queueLength--;
+            }
+
+            traversal.Add(currentLevelValues);
+        }
+
+        return traversal;
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
