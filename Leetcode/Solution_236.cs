7e27017c62939976723a89f25da9deb2f5fffcb1
Sergey Korepanov <igzhestik@mail.ru>
2025-08-22 17:32:23 +0500

    Solution 236. Lowest Common Ancestor of a Binary Tree

---
+++ Leetcode/Solution_236.cs
@@ -0,0 +1,65 @@
+﻿// 236. Lowest Common Ancestor of a Binary Tree
+public class Solution_236
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root.val == p.val || root.val == q.val)
+        {
+            return root;
+        }
+
+        var isPInLeft = false;
+        var isQInLeft = false;
+
+        FindValues(root.left, p.val, q.val, ref isPInLeft, ref isQInLeft);
+
+        if (isPInLeft && isQInLeft)
+        {
+            return LowestCommonAncestor(root.left, p, q);
+        }
+
+        if (!isPInLeft && !isQInLeft)
+        {
+            return LowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+
+    private void FindValues(TreeNode node, int p, int q, ref bool hasP, ref bool hasQ)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        if (node.val == p)
+        {
+            hasP = true;
+        }
+        else if (node.val == q)
+        {
+            hasQ = true;
+        }
+
+        if (hasP && hasQ)
+        {
+            return;
+        }
+
+        FindValues(node.left, p, q, ref hasP, ref hasQ);
+        FindValues(node.right, p, q, ref hasP, ref hasQ);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
