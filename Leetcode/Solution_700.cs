6de9f459feeda366c700f11fc07f2e3b186687ec
Sergey Korepanov <igzhestik@mail.ru>
2025-08-28 16:08:00 +0500

    Solution 700. Search in a Binary Search Tree

---
+++ Leetcode/Solution_700.cs
@@ -0,0 +1,34 @@
+﻿// 700. Search in a Binary Search Tree
+public class Solution_700
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root is null)
+        {
+            return null;
+        }
+
+        if (root.val == val)
+        {
+            return root;
+        }
+
+        return root.val > val
+            ? SearchBST(root.left, val)
+            : SearchBST(root.right, val);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
