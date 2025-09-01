081cec3dccf98086ea5d6ba4eb1d3038d11de6dd
Sergey Korepanov <igzhestik@mail.ru>
2025-09-01 19:50:36 +0500

    Solution 509. Fibonacci Number

---
+++ Leetcode/Solution_509.cs
@@ -0,0 +1,27 @@
+﻿// 509. Fibonacci Number
+public class Solution_509
+{
+    private readonly Dictionary<int, int> _cache = new();
+
+    public int Fib(int n)
+    {
+        switch (n)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+        }
+
+        if (_cache.TryGetValue(n, out var cachedFib))
+        {
+            return cachedFib;
+        }
+
+        var fib = Fib(n - 1) + Fib(n - 2);
+
+        _cache.Add(n, fib);
+
+        return fib;
+    }
+}
