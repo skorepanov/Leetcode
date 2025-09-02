b7e425d53245763af40e8ba42ef9789040b93781
Sergey Korepanov <igzhestik@mail.ru>
2025-09-02 17:59:01 +0500

    Solution 70. Climbing Stairs

---
+++ Leetcode/Solution_70.cs
@@ -0,0 +1,27 @@
+﻿// 70. Climbing Stairs
+public class Solution_70
+{
+    private readonly Dictionary<int, int>  _cache = new();
+
+    public int ClimbStairs(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+        }
+
+        if (_cache.TryGetValue(n, out var cachedCount))
+        {
+            return cachedCount;
+        }
+
+        var count = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+
+        _cache.Add(n, count);
+
+        return count;
+    }
+}
