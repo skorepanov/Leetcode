a10bcd19c369373621b4112e6d966cda8d421b06
Sergey Korepanov <igzhestik@mail.ru>
2025-10-07 15:07:42 +0500

    Solution 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Leetcode/Solution_235.cs
@@ -0,0 +1,30 @@
+﻿// 235. Lowest Common Ancestor of a Binary Search Tree
+public class Solution_235
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if ((p.val <= root.val && root.val <= q.val)
+         || (q.val <= root.val && root.val <= p.val))
+        {
+            return root;
+        }
+
+        return p.val < root.val && q.val < root.val
+            ? LowestCommonAncestor(root.left, p, q)
+            : LowestCommonAncestor(root.right, p, q);
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
