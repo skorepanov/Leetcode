c41d5c79aff55e73aeb7cd149026b60798ebacde
Sergey Korepanov <igzhestik@mail.ru>
2025-10-14 11:59:28 +0500

    Solution 589. N-ary Tree Preorder Traversal

---
+++ Leetcode/Solution_589.cs
@@ -0,0 +1,44 @@
+﻿// 589. N-ary Tree Preorder Traversal
+public class Solution_589
+{
+    public IList<int> Preorder(Node root)
+    {
+        var traversal = new List<int>();
+        Traverse(root, traversal);
+        return traversal;
+    }
+
+    private void Traverse(Node node, List<int> traversal)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        traversal.Add(node.val);
+
+        foreach (var nodeChild in node.children)
+        {
+            Traverse(nodeChild, traversal);
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
