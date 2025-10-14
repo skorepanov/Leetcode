f449701e4433e0808e6e94837630e464b5d9f36e
Sergey Korepanov <igzhestik@mail.ru>
2025-10-14 12:13:37 +0500

    Solution 590. N-ary Tree Postorder Traversal

---
+++ Leetcode/Solution_590.cs
@@ -0,0 +1,44 @@
+﻿// 590. N-ary Tree Postorder Traversal
+public class Solution_590
+{
+    public IList<int> Postorder(Node root)
+    {
+        var traversal = new List<int>();
+        Traverse(root, traversal);
+        return traversal;
+    }
+
+    private void Traverse(Node node, List<int> traversal)
+    {
+        if (node is null)
+        {
+            return;
+        }
+
+        foreach (var child in node.children)
+        {
+            Traverse(child, traversal);
+        }
+
+        traversal.Add(node.val);
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
