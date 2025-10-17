5043548af8b84ffcdd994ffbec0ba43b9f17edf4
Sergey Korepanov <igzhestik@mail.ru>
2025-10-17 15:28:24 +0500

    Solution 1083. Sales Analysis II

---
+++ Leetcode/Solution_1083.sql
@@ -0,0 +1,13 @@
+﻿select distinct
+    s.buyer_id
+from Sales s
+    inner join Product p
+        on s.product_id = p.product_id
+where p.product_name = 'S8'
+    except
+select
+    distinct s.buyer_id
+from Sales s
+    inner join Product p
+        on s.product_id = p.product_id
+where p.product_name = 'iPhone'
