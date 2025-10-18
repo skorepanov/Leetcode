1ac3452535bb3622b9b88dc83d5f117bdff585a7
Sergey Korepanov <igzhestik@mail.ru>
2025-10-18 21:33:33 +0500

    Solution 597. Friend Requests I: Overall Acceptance Rate

---
+++ Leetcode/Solution_597.sql
@@ -0,0 +1,20 @@
+﻿-- 597. Friend Requests I: Overall Acceptance Rate
+with requests as (
+    select distinct
+        sender_id,
+        send_to_id
+    from FriendRequest
+), accepted as (
+    select distinct
+        requester_id,
+        accepter_id
+    from RequestAccepted
+)
+select
+    case
+        when (select count(*) from requests) = 0 then 0
+        else
+            round(
+                cast((select count(*) from accepted) as decimal) / (select count(*) from requests),
+                2)
+    end as accept_rate
