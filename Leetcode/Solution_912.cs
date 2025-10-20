d12231d6ecab1dfede1ab29a7965791147ea7469
Sergey Korepanov <igzhestik@mail.ru>
2025-10-20 20:04:33 +0500

    Solution 912. Sort an Array. Реализован merge sort

--- Leetcode/Solution_912.cs
+++ Leetcode/Solution_912.cs
@@ -1,9 +1,10 @@
 ﻿// 912. Sort an Array
-public class Solution_912
+
+#region Вариант 1 - Heap sort - Runtime 79 ms, Beats 76.02%
+public class Solution_912_1
 {
     public int[] SortArray(int[] nums)
     {
-        // using heap sort
         for (var i = nums.Length / 2 - 1; i >= 0; i--)
         {
             maxHeapify(nums, nums.Length, i);
@@ -41,3 +42,67 @@ public class Solution_912
         }
     }
 }
+#endregion
+
+#region Вариант 2 - Merge sort - Runtime 133 ms, Beats 32.12%
+public class Solution_912_2
+{
+    public int[] SortArray(int[] nums)
+    {
+        MergeSort(nums);
+        return nums;
+    }
+
+    private void MergeSort(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return;
+        }
+
+        var middle = nums.Length / 2;
+
+        var left = new int[middle];
+        var right = new int[nums.Length - middle];
+
+        for (var i = 0; i < middle; i++)
+        {
+            left[i] = nums[i];
+        }
+
+        for (var i = middle; i < nums.Length; i++)
+        {
+            right[i - middle] = nums[i];
+        }
+
+        MergeSort(left);
+        MergeSort(right);
+
+        Merge(nums, left, right);
+    }
+
+    private void Merge(int[] nums, int[] left, int[] right)
+    {
+        var numIndex = 0;
+        var leftIndex = 0;
+        var rightIndex = 0;
+
+        while (leftIndex < left.Length && rightIndex < right.Length)
+        {
+            nums[numIndex++] = left[leftIndex] < right[rightIndex]
+                ? left[leftIndex++]
+                : right[rightIndex++];
+        }
+
+        while (leftIndex < left.Length)
+        {
+            nums[numIndex++] = left[leftIndex++];
+        }
+
+        while (rightIndex < right.Length)
+        {
+            nums[numIndex++] = right[rightIndex++];
+        }
+    }
+}
+#endregion
