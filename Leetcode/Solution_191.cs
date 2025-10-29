216afb54682b51875f3632360cf018988e5280f8
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 19:06:01 +0500

    Solution 191. Number of 1 Bits

---
+++ Leetcode/Solution_191.cs
@@ -0,0 +1,20 @@
+﻿// 191. Number of 1 Bits
+public class Solution_191
+{
+    public int HammingWeight(int n)
+    {
+        var countOf1 = 0;
+
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                countOf1++;
+            }
+
+            n /= 2;
+        }
+
+        return countOf1;
+    }
+}
