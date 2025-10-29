597fc40ca4bc99b6ab62178f5878e109a283534a
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 19:54:16 +0500

    Solution 371. Sum of Two Integers

---
+++ Leetcode/Solution_371.cs
@@ -0,0 +1,16 @@
+﻿// 371. Sum of Two Integers
+public class Solution_371
+{
+    public int GetSum(int a, int b)
+    {
+        while (b != 0)
+        {
+            var answer = a ^ b;
+            var carry = (a & b) << 1;
+            a = answer;
+            b = carry;
+        }
+
+        return a;
+    }
+}
