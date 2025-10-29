b5b72d3354ec4ac29d3043d46dc8ddb2a98b8c6d
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 20:51:04 +0500

    Solution 338. Counting Bits

---
+++ Leetcode/Solution_338.cs
@@ -0,0 +1,16 @@
+﻿// 338. Counting Bits
+public class Solution_338
+{
+    public int[] CountBits(int n)
+    {
+        var bits = new int[n + 1];
+
+        for (var i = 1; i < bits.Length; i++)
+        {
+            var index = i & (i - 1);
+            bits[i] = bits[index] + 1;
+        }
+
+        return bits;
+    }
+}
