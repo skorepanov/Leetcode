d652e7f50d2f2cb1793e97a41730d75a98b47350
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 20:19:42 +0500

    Solution 89. Gray Code

---
+++ Leetcode/Solution_89.cs
@@ -0,0 +1,18 @@
+﻿// 89. Gray Code
+public class Solution_89
+{
+    public IList<int> GrayCode(int n)
+    {
+        var grayCode = new List<int>();
+
+        var sequenceLength = 1 << n;
+
+        for (var i = 0; i < sequenceLength; i++)
+        {
+            var num = i ^ (i >> 1);
+            grayCode.Add(num);
+        }
+
+        return grayCode;
+    }
+}
