cade715ed226fa74c44b4edc6f6fa6664b9c648b
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 18:12:27 +0500

    Solution 405. Convert a Number to Hexadecimal

---
+++ Leetcode/Solution_405.cs
@@ -0,0 +1,31 @@
+﻿// 405. Convert a Number to Hexadecimal
+public class Solution_405
+{
+    public string ToHex(int num)
+    {
+        if (num == 0)
+        {
+            return "0";
+        }
+
+        const int BASE = 16;
+        var hexChars = "0123456789abcdef".ToCharArray();
+
+        // конвертировать число в беззнаковое 32-битное представление
+        // используется метод two's complement
+        var unsignedNum = (uint)num;
+
+        var numBase16 = string.Empty;
+
+        while (unsignedNum > 0)
+        {
+            var remainder = (int)(unsignedNum % BASE);
+            unsignedNum /= BASE;
+
+            var hexChar = hexChars[remainder];
+            numBase16 = hexChar + numBase16;
+        }
+
+        return numBase16;
+    }
+}
