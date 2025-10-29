adb3700d1253b71bd1d9a3fcf1b3ae59a2ef113b
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 20:32:59 +0500

    Solution 201. Bitwise AND of Numbers Range

---
+++ Leetcode/Solution_201.cs
@@ -0,0 +1,17 @@
+﻿// 201. Bitwise AND of Numbers Range
+public class Solution_201
+{
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        var shift = 0;
+
+        while (left < right)
+        {
+            left = left >> 1;
+            right = right >> 1;
+            shift++;
+        }
+
+        return right << shift;
+    }
+}
