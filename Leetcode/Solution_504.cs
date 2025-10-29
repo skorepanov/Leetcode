798ea3023a47a74cd8adacc8a4ebccb13b67fc76
Sergey Korepanov <igzhestik@mail.ru>
2025-10-29 17:06:17 +0500

    Solution 504. Base 7

---
+++ Leetcode/Solution_504.cs
@@ -0,0 +1,24 @@
+﻿// 504. Base 7
+public class Solution_504
+{
+    public string ConvertToBase7(int num)
+    {
+        if (num == 0)
+        {
+            return "0";
+        }
+
+        var numberBase7 = string.Empty;
+        var absoluteNumber = Math.Abs(num);
+
+        while (absoluteNumber != 0)
+        {
+            numberBase7 = (absoluteNumber % 7) + numberBase7;
+            absoluteNumber /= 7;
+        }
+
+        return num > 0
+            ? numberBase7
+            : "-" + numberBase7;
+    }
+}
