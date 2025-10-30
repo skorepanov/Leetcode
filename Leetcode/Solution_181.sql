b98d4c1730f04cd40089493b78515d7fd9a46582
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 19:11:20 +0500

    Solution 181. Employees Earning More Than Their Managers

---
+++ Leetcode/Solution_181.sql
@@ -0,0 +1,6 @@
+﻿-- 181. Employees Earning More Than Their Managers
+select
+    subordinate.name as Employee
+from Employee subordinate
+    inner join Employee manager on subordinate.managerId = manager.id
+where subordinate.salary > manager.salary
