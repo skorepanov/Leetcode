4a8fb2ce41f84fb066b7e8fd3cbc2a9175ae941d
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 19:05:34 +0500

    Solution 160. Intersection of Two Linked Lists

---
+++ Leetcode/Solution_160.cs
@@ -0,0 +1,41 @@
+﻿// 160. Intersection of Two Linked Lists
+public class Solution_160
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        var pointerA = headA;
+        var pointerB = headB;
+
+        var nodesOfB = new HashSet<ListNode>();
+
+        while (pointerB != null)
+        {
+            nodesOfB.Add(pointerB);
+            pointerB = pointerB.next;
+        }
+
+        while (pointerA != null)
+        {
+            if (nodesOfB.Contains(pointerA))
+            {
+                return pointerA;
+            }
+
+            pointerA = pointerA.next;
+        }
+
+        return null;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
