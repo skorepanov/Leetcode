8b51f9ff79799a9ba439da803dc6f8cef4aca7ab
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 19:01:18 +0500

    Solution 141. Linked List Cycle

---
+++ Leetcode/Solution_141.cs
@@ -0,0 +1,39 @@
+﻿// 141. Linked List Cycle
+public class Solution_141
+{
+    public bool HasCycle(ListNode head)
+    {
+        if (head?.next is null)
+        {
+            return false;
+        }
+
+        var fastPointer = head.next.next;
+        var slowPointer = head.next;
+
+        while (fastPointer?.next != null)
+        {
+            if (slowPointer == fastPointer)
+            {
+                return true;
+            }
+
+            fastPointer = fastPointer.next?.next;
+            slowPointer = slowPointer.next;
+        }
+
+        return false;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
