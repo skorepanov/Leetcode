096c20a5577e4e225a02d928e1adc50163f79586
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 19:15:59 +0500

    Solution 185. Department Top Three Salaries

---
+++ Leetcode/Solution_185.sql
@@ -0,0 +1,17 @@
+﻿-- 185. Department Top Three Salaries
+with ranks as (
+    select
+        id,
+        name as employee,
+        salary,
+        departmentId,
+        dense_rank() over (partition by departmentId order by salary desc) as rank
+    from Employee
+)
+select
+    d.name as department,
+    r.employee,
+    r.salary
+from ranks r
+    inner join Department d on r.departmentId = d.id
+where r.rank <= 3
