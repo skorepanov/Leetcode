f4f9691921433d44aa2ea16015cf93b31951b31c
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 18:51:25 +0500

    Solution 27. Remove Element

---
+++ Leetcode/Solution_27.cs
@@ -0,0 +1,19 @@
+﻿// 27. Remove Element
+public class Solution_27
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var otherNumberIndex = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                (nums[i], nums[otherNumberIndex]) = (nums[otherNumberIndex], nums[i]);
+                otherNumberIndex++;
+            }
+        }
+
+        return otherNumberIndex;
+    }
+}
