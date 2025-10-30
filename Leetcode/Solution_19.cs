6f6b1e545bd57b70b3761484cee610e98e4c1263
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 18:41:34 +0500

    Solution 19. Remove Nth Node From End of List

---
+++ Leetcode/Solution_19.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var nodesAsList = new List<ListNode>();
+
+        var pointer = head;
+
+        while (pointer != null)
+        {
+            nodesAsList.Add(pointer);
+            pointer = pointer.next;
+        }
+
+        if (nodesAsList.Count == n)
+        {
+            return head.next;
+        }
+
+        var previousNode = nodesAsList[nodesAsList.Count - n - 1];
+
+        previousNode.next = previousNode.next.next;
+
+        return head;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
