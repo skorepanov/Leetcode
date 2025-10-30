701b23ee01196b9dd8869454d69a0ad6b81ebaed
Sergey Korepanov <igzhestik@mail.ru>
2025-10-30 19:10:14 +0500

    Solution 180. Consecutive Numbers

---
+++ Leetcode/Solution_180.sql
@@ -0,0 +1,6 @@
+﻿-- 180. Consecutive Numbers
+select distinct
+    l1.num as ConsecutiveNums
+from Logs l1
+    inner join Logs l2 on l1.id = l2.id - 1 and l1.num = l2.num
+    inner join Logs l3 on l2.id = l3.id - 1 and l2.num = l3.num
