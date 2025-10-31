39cdc89fdca9ec4fefeec00b41bc8ae62d81aa5e
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:10:26 +0500

    Solution 1241. Number of Comments per Post

---
+++ Leetcode/Solution_1241.sql
@@ -0,0 +1,9 @@
+﻿-- 1241. Number of Comments per Post
+select
+    posts.sub_id as post_id,
+    count(distinct comments.sub_id) as number_of_comments
+from Submissions posts
+    left join Submissions comments on posts.sub_id = comments.parent_id
+where posts.parent_id is null
+group by posts.sub_id
+order by posts.sub_id
