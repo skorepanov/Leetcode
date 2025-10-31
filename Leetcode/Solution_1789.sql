be1e4f78a79a466c310d5665fe60716965ca5408
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:42:23 +0500

    Solution 1789. Primary Department for Each Employee

---
+++ Leetcode/Solution_1789.sql
@@ -0,0 +1,13 @@
+﻿-- 1789. Primary Department for Each Employee
+select
+    employee_id,
+    department_id
+from Employee
+where primary_flag = 'Y'
+    or employee_id in (
+        select
+            employee_id
+        from Employee
+        group by employee_id
+        having count(*) = 1
+    )
