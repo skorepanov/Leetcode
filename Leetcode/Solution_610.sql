496a72717298b30ffbbdc1030498dc5ced028a90
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:11:54 +0500

    Solution 610. Triangle Judgement

---
+++ Leetcode/Solution_610.sql
@@ -0,0 +1,11 @@
+﻿-- 610. Triangle Judgement
+select
+    x,
+    y,
+    z,
+    case
+        when x + y > z and x + z > y and y + z > x
+            then 'Yes'
+            else 'No'
+        end as triangle
+from Triangle
