ed74a0736cabf462344a045609c89b0497c710b7
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:47:06 +0500

    Solution 905. Sort Array By Parity

---
+++ Leetcode/Solution_905.cs
@@ -0,0 +1,19 @@
+﻿// 905. Sort Array By Parity
+public class Solution_905
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        var evenIndex = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                (nums[i], nums[evenIndex]) = (nums[evenIndex], nums[i]);
+                evenIndex++;
+            }
+        }
+
+        return nums;
+    }
+}
