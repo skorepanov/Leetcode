eea44208d4ab29ef65e9b6da258c30e122225b79
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:46:19 +0500

    Solution 1934. Confirmation Rate

---
+++ Leetcode/Solution_1934.sql
@@ -0,0 +1,20 @@
+﻿-- 1934. Confirmation Rate
+select
+    s.user_id,
+    round(coalesce(conf_confs.count, 0)::decimal / coalesce(all_confs.count, 1)::decimal, 2) as confirmation_rate
+from Signups s
+    left join (
+        select
+            user_id,
+            count(*) as count
+        from Confirmations
+        group by user_id
+    ) as all_confs on s.user_id = all_confs.user_id
+    left join (
+        select
+            user_id,
+            count(*) as count
+        from Confirmations
+        where action = 'confirmed'
+        group by user_id
+    ) as conf_confs on s.user_id = conf_confs.user_id
