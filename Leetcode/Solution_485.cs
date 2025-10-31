24bfc410d91227989c3f12858bc862c150c45113
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 15:51:01 +0500

    Solution 485. Max Consecutive Ones

---
+++ Leetcode/Solution_485.cs
@@ -0,0 +1,28 @@
+﻿// 485. Max Consecutive Ones
+public class Solution_485
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        var max = 0;
+        var current = 0;
+
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                current++;
+
+                if (current > max)
+                {
+                    max = current;
+                }
+            }
+            else
+            {
+                current = 0;
+            }
+        }
+
+        return max;
+    }
+}
