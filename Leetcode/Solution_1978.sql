3d84a5b2bca00775de37212330f919d44be5177d
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:48:40 +0500

    Solution 1978. Employees Whose Manager Left the Company

---
+++ Leetcode/Solution_1978.sql
@@ -0,0 +1,9 @@
+﻿-- 1978. Employees Whose Manager Left the Company
+select
+    subordinates.employee_id
+from Employees subordinates
+    left join Employees managers on subordinates.manager_id = managers.employee_id
+where subordinates.manager_id is not null
+    and subordinates.salary < 30000
+    and managers.employee_id is null
+order by subordinates.employee_id
