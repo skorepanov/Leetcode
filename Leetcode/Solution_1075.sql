c56c833a5b5c61fd7c697afa0e59bf2eeef1b6c0
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:57:01 +0500

    Solution 1075. Project Employees I

---
+++ Leetcode/Solution_1075.sql
@@ -0,0 +1,7 @@
+﻿-- 1075. Project Employees I
+select
+    p.project_id,
+    round(avg(e.experience_years), 2) as average_years
+from Project p
+    inner join Employee e on p.employee_id = e.employee_id
+group by p.project_id
