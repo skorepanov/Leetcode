45bbf75172b6b836fe650217636fe7a736110619
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:05:05 +0500

    Solution 1193. Monthly Transactions I

---
+++ Leetcode/Solution_1193.sql
@@ -0,0 +1,10 @@
+﻿-- 1193. Monthly Transactions I
+select
+    to_char(date_trunc('month', trans_date), 'YYYY-MM') as month,
+    country,
+    count(*) as trans_count,
+    count(*) filter (where state = 'approved') as approved_count,
+    sum(amount) as trans_total_amount,
+    coalesce(sum(amount) filter (where state = 'approved'), 0) as approved_total_amount
+from Transactions
+group by month, country
