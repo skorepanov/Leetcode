9a324087f924bf7aac159d390e98fd9b041b991c
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:08:17 +0500

    Solution 602. Friend Requests II: Who Has the Most Friends

---
+++ Leetcode/Solution_602.sql
@@ -0,0 +1,21 @@
+﻿-- 602. Friend Requests II: Who Has the Most Friends
+with all_friends as (
+    select
+        requester_id as id,
+        count(accepter_id) as count
+    from RequestAccepted
+    group by requester_id
+        union all
+    select
+        accepter_id as id,
+        count(requester_id) as count
+    from RequestAccepted
+    group by accepter_id
+)
+select
+    id,
+    sum(count) as num
+from all_friends
+group by id
+order by num desc
+fetch first 1 row with ties
