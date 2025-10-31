c05a62d17639b725c360f9b3ddd0cb696460e203
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:41:45 +0500

    Solution 1731. The Number of Employees Which Report to Each Employee

---
+++ Leetcode/Solution_1731.sql
@@ -0,0 +1,10 @@
+﻿-- 1731. The Number of Employees Which Report to Each Employee
+select
+    managers.employee_id,
+    managers.name,
+    count(subordinates.employee_id) as reports_count,
+    round(avg(subordinates.age)) as average_age
+from Employees managers
+    inner join Employees subordinates on managers.employee_id = subordinates.reports_to
+group by managers.employee_id, managers.name
+order by managers.employee_id
