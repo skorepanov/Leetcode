82800acc34bad9aabc120dc338f52dc31e3a3b78
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:58:00 +0500

    Solution 1089. Duplicate Zeros

---
+++ Leetcode/Solution_1089.cs
@@ -0,0 +1,20 @@
+﻿// 1089. Duplicate Zeros
+public class Solution_1089
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] == 0)
+            {
+                for (var right = arr.Length - 1; right > i; right--)
+                {
+                    arr[right] = arr[right - 1];
+                }
+
+                arr[i + 1] = 0;
+                i++;
+            }
+        }
+    }
+}
