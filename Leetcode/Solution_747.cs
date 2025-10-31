bcc3e33529c9eed062b4e712f6eb543f3827e255
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:45:47 +0500

    Solution 747. Largest Number At Least Twice of Others

---
+++ Leetcode/Solution_747.cs
@@ -0,0 +1,26 @@
+﻿// 747. Largest Number At Least Twice of Others
+public class Solution_747
+{
+    public int DominantIndex(int[] nums)
+    {
+        var indexOfLargest = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[indexOfLargest])
+            {
+                indexOfLargest = i;
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i != indexOfLargest && nums[i] * 2 > nums[indexOfLargest])
+            {
+                return -1;
+            }
+        }
+
+        return indexOfLargest;
+    }
+}
