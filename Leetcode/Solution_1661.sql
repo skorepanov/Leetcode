6b67251ab3a898a4a8bb2938863812bab37330f3
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:36:50 +0500

    Solution 1661. Average Time of Process per Machine

---
+++ Leetcode/Solution_1661.sql
@@ -0,0 +1,17 @@
+﻿-- 1661. Average Time of Process per Machine
+with timestamps as (
+    select
+        a1.machine_id,
+        a1.process_id,
+        a2.timestamp - a1.timestamp as processing_time
+    from activity a1
+    inner join activity a2 on a1.machine_id = a2.machine_id
+        and a1.process_id = a2.process_id
+        and a2.activity_type = 'end'
+    where a1.activity_type = 'start'
+)
+select
+    machine_id,
+    round(avg(processing_time)::numeric, 3) as processing_time
+from timestamps
+group by machine_id
