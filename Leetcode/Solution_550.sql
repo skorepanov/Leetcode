3480fa5343dd4a86a5a57cd9f698f7e8e7398843
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 15:58:02 +0500

    Solution 550. Game Play Analysis IV

---
+++ Leetcode/Solution_550.sql
@@ -0,0 +1,11 @@
+﻿-- 550. Game Play Analysis IV
+select
+    round(count(*) / (select count(distinct player_id) from Activity)::decimal, 2) as fraction
+from Activity
+where (player_id, event_date - interval '1 DAY') in (
+    select
+        player_id,
+        min(event_date)
+    from Activity
+    group by player_id
+)
