5cd0ecb334bb1b71658eddb3c9b04df1210b1eaf
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 15:10:28 +0500

    Solution 1142. User Activity for the Past 30 Days II

---
+++ Leetcode/Solution_1142.sql
@@ -0,0 +1,12 @@
+﻿-- 1142. User Activity for the Past 30 Days II
+with sessions as (
+    select
+        user_id,
+        count(distinct session_id) as session_count
+    from Activity
+    where activity_date between '20190628' and '20190727'
+    group by user_id
+)
+select
+    coalesce(round(avg(session_count), 2), 0) as average_sessions_per_user
+from sessions
