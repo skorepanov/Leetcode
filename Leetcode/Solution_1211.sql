e93ebbdb8feda80f92a1de5aa310b2726ec03be2
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:09:30 +0500

    Solution 1211. Queries Quality and Percentage

---
+++ Leetcode/Solution_1211.sql
@@ -0,0 +1,12 @@
+﻿-- 1211. Queries Quality and Percentage
+select
+    query_name,
+    round(
+        sum(rating / position::decimal) / count(*),
+        2) as quality,
+    round(
+        count(*) filter (where rating < 3) / count(*)::decimal * 100,
+        2) as poor_query_percentage
+from Queries
+where query_name is not null
+group by query_name
