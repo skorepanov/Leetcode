c0e8d3970407648d785ff093af34df7a672d8311
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:55:10 +0500

    Solution 1051. Height Checker

---
+++ Leetcode/Solution_1051.cs
@@ -0,0 +1,19 @@
+﻿// 1051. Height Checker
+public class Solution_1051
+{
+    public int HeightChecker(int[] heights)
+    {
+        var sortedHeights = heights.OrderBy(h => h).ToArray();
+        var indexCount = 0;
+
+        for (var i = 0; i < heights.Length; i++)
+        {
+            if (heights[i] != sortedHeights[i])
+            {
+                indexCount++;
+            }
+        }
+
+        return indexCount;
+    }
+}
