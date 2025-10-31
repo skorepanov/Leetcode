e6b1392b03f34118e6b1e0c6adb25c7d0c62923c
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:00:13 +0500

    Solution 1141. User Activity for the Past 30 Days I

---
+++ Leetcode/Solution_1141.sql
@@ -0,0 +1,7 @@
+﻿-- 1141. User Activity for the Past 30 Days I
+select
+    activity_date as day,
+    count(distinct user_id) as active_users
+from Activity
+where activity_date between ('20190727'::date - interval '29 DAYS') and '20190727'::date
+group by activity_date
