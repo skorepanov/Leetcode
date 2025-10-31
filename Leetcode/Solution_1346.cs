d1feca19ab1561ffafd57f914d1ed034b23d5cf7
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:18:50 +0500

    Solution 1346. Check If N and Its Double Exist

---
+++ Leetcode/Solution_1346.cs
@@ -0,0 +1,19 @@
+﻿// 1346. Check If N and Its Double Exist
+public class Solution_1346
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        for (var i = 0; i < arr.Length; i++)
+        {
+            for (var j = 0; j < arr.Length; j++)
+            {
+                if (i != j && arr[i] == 2 * arr[j])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
