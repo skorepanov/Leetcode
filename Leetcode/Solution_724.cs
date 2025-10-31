e0f48e184619cf633bb058621485ea521da40161
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:45:02 +0500

    Solution 724. Find Pivot Index

---
+++ Leetcode/Solution_724.cs
@@ -0,0 +1,33 @@
+﻿// 724. Find Pivot Index
+public class Solution_724
+{
+    public int PivotIndex(int[] nums)
+    {
+        var rightSum = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        if (rightSum == 0)
+        {
+            return 0;
+        }
+
+        var leftSum = 0;
+
+        for (var pivot = 1; pivot < nums.Length; pivot++)
+        {
+            leftSum += nums[pivot - 1];
+            rightSum -= nums[pivot];
+
+            if (leftSum == rightSum)
+            {
+                return pivot;
+            }
+        }
+
+        return -1;
+    }
+}
