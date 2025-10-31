3276c2b0ac16156435601c98fb59471428639209
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:12:28 +0500

    Solution 1280. Students and Examinations

---
+++ Leetcode/Solution_1280.sql
@@ -0,0 +1,12 @@
+﻿-- 1280. Students and Examinations
+select
+    st.student_id,
+    st.student_name,
+    sj.subject_name,
+    count(*) filter (where e.student_id is not null) as attended_exams
+from students st
+    inner join subjects sj on true
+    left join examinations e on st.student_id = e.student_id
+        and sj.subject_name = e.subject_name
+group by st.student_id, st.student_name, sj.subject_name
+order by st.student_id, sj.subject_name
