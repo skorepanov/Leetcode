6866866667006a7cf65e59ea76726c8baea439e7
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:48:02 +0500

    Solution 941. Valid Mountain Array

---
+++ Leetcode/Solution_941.cs
@@ -0,0 +1,43 @@
+﻿// 941. Valid Mountain Array
+public class Solution_941
+{
+    public bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3)
+        {
+            return false;
+        }
+
+        if (arr[1] < arr[0])
+        {
+            return false;
+        }
+
+        var shouldIncrease = true;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] == arr[i - 1])
+            {
+                return false;
+            }
+
+            if (shouldIncrease && arr[i - 1] > arr[i])
+            {
+                shouldIncrease = false;
+            }
+
+            if (!shouldIncrease && arr[i - 1] < arr[i])
+            {
+                return false;
+            }
+        }
+
+        if (shouldIncrease)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
