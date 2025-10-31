4f37d014d21855fce1593ccee9abe2b9c30af13b
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:49:24 +0500

    Solution 1991. Find the Middle Index in Array

---
+++ Leetcode/Solution_1991.cs
@@ -0,0 +1,33 @@
+﻿// 1991. Find the Middle Index in Array
+public class Solution_1991
+{
+    public int FindMiddleIndex(int[] nums)
+    {
+        var rightSum = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        if (rightSum == 0)
+        {
+            return 0;
+        }
+
+        var leftSum = 0;
+
+        for (var middleIndex = 1; middleIndex < nums.Length; middleIndex++)
+        {
+            leftSum += nums[middleIndex - 1];
+            rightSum -= nums[middleIndex];
+
+            if (leftSum == rightSum)
+            {
+                return middleIndex;
+            }
+        }
+
+        return -1;
+    }
+}
