93b09025c83ad4e8ac81b4eefbfc0aeb1de67558
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 15:59:48 +0500

    Solution 570. Managers with at Least 5 Direct Reports

---
+++ Leetcode/Solution_570.sql
@@ -0,0 +1,10 @@
+﻿-- 570. Managers with at Least 5 Direct Reports
+select name
+from Employee
+where id in (
+    select managerId
+    from Employee
+    where managerId is not null
+    group by managerId
+    having count(*) >= 5
+)
