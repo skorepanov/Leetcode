1adea11f7121b4ace89e8145847532e14017dbf1
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:25:08 +0500

    Solution 1484. Group Sold Products By The Date

---
+++ Leetcode/Solution_1485.sql
@@ -0,0 +1,8 @@
+﻿-- 1484. Group Sold Products By The Date
+select
+    sell_date,
+    count(distinct product) as num_sold,
+    string_agg(distinct product, ',') as products
+from Activities
+group by sell_date
+order by sell_date
