b92b762643405a29fa7e24b44d510702d725720c
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:47:07 +0500

    Solution 1965. Employees With Missing Information

---
+++ Leetcode/Solution_1965.sql
@@ -0,0 +1,7 @@
+﻿-- 1965. Employees With Missing Information
+select
+    coalesce(e.employee_id, s.employee_id) as employee_id
+from Employees e
+    full join Salaries s on e.employee_id = s.employee_id
+where e.employee_id is null or s.employee_id is null
+order by employee_id
