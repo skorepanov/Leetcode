601e722acf75748a53e18c44a1ad8ab8ffc1e749
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:23:24 +0500

    Solution 1440. Evaluate Boolean Expression

---
+++ Leetcode/Solution_1440.sql
@@ -0,0 +1,14 @@
+﻿-- 1440. Evaluate Boolean Expression
+select
+    e.left_operand,
+    e.operator,
+    e.right_operand,
+    case
+        when (e.operator = '>' and v_left.value > v_right.value) then 'true'
+        when (e.operator = '<' and v_left.value < v_right.value) then 'true'
+        when (e.operator = '=' and v_left.value = v_right.value) then 'true'
+        else 'false'
+    end as value
+from Expressions e
+    inner join Variables v_left on e.left_operand = v_left.name
+    inner join Variables v_right on e.right_operand = v_right.name
