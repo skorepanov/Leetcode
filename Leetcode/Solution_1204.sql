ebfb02a6236638767c6b7e87eab9430054460595
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:08:13 +0500

    Solution 1204. Last Person to Fit in the Bus

---
+++ Leetcode/Solution_1204.sql
@@ -0,0 +1,14 @@
+﻿-- 1204. Last Person to Fit in the Bus
+with weights as (
+    select
+        person_id,
+        person_name,
+        sum(weight) over (order by turn) as sum
+    from Queue
+)
+select
+    person_name
+from weights
+where sum <= 1000
+order by sum desc
+limit 1
