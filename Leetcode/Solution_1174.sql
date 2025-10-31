e997e407cfe7d51134ed7bdf268703ba010ba818
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:03:42 +0500

    Solution 1174. Immediate Food Delivery II

---
+++ Leetcode/Solution_1174.sql
@@ -0,0 +1,10 @@
+﻿-- 1174. Immediate Food Delivery II
+select
+    round(avg(percentage::decimal) * 100, 2) as immediate_percentage
+from (
+    select
+        customer_id,
+        case when min(order_date) = min(customer_pref_delivery_date) then 1 else 0 end as percentage
+    from Delivery
+    group by customer_id
+)
