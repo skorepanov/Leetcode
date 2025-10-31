bd5c6afe1cdfc6257b58385d89ad473250f56c4c
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:35:17 +0500

    Solution 1633. Percentage of Users Attended a Contest

---
+++ Leetcode/Solution_1633.sql
@@ -0,0 +1,7 @@
+﻿-- 1633. Percentage of Users Attended a Contest
+select
+    r.contest_id,
+    round(count(*) / (select count(*) from Users)::decimal * 100, 2) as percentage
+from Register r
+group by r.contest_id
+order by percentage desc, r.contest_id
