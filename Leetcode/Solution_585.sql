904b3696fbd488a3f6116e3abcc57a1f3c280255
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:03:23 +0500

    Solution 585. Investments in 2016

---
+++ Leetcode/Solution_585.sql
@@ -0,0 +1,17 @@
+﻿-- 585. Investments in 2016
+select
+    round(sum(tiv_2016)::decimal, 2) as tiv_2016
+from Insurance i1
+where exists (
+    select
+        i2.tiv_2015
+    from Insurance i2
+    where i1.pid != i2.pid and i1.tiv_2015 = i2.tiv_2015
+)
+    and (i1.lat, i1.lon) not in (
+        select
+            i3.lat,
+            i3.lon
+        from Insurance i3
+        where i1.pid != i3.pid
+    )
