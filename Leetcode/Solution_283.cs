852b0d7c60d01fc7b71a28b968ce971ec6381068
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 15:41:45 +0500

    Solution 283. Move Zeroes

---
+++ Leetcode/Solution_283.cs
@@ -0,0 +1,21 @@
+﻿// 283. Move Zeroes
+public class Solution_283
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var zeroIndex = -1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0 && zeroIndex == -1)
+            {
+                zeroIndex = i;
+            }
+            else if (nums[i] != 0 && zeroIndex > -1)
+            {
+                (nums[i], nums[zeroIndex]) = (nums[zeroIndex], nums[i]);
+                zeroIndex++;
+            }
+        }
+    }
+}
