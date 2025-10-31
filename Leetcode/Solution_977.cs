d8b8558b7f8a9f065a55092a7f772f6dded308dc
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:50:22 +0500

    Solution 977. Squares of a Sorted Array

---
+++ Leetcode/Solution_977.cs
@@ -0,0 +1,27 @@
+﻿// 977. Squares of a Sorted Array
+public class Solution_977
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        var numsLength = nums.Length;
+        var sortedNums = new int[numsLength];
+        var left = 0;
+        var right = numsLength - 1;
+
+        for (var i = numsLength - 1; i >= 0; i--)
+        {
+            if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+            {
+                sortedNums[i] = nums[left] * nums[left];
+                left++;
+            }
+            else
+            {
+                sortedNums[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return sortedNums;
+    }
+}
