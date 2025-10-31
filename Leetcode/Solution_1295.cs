52de9b671682db78d15646249e8d3ee080c7d952
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:13:38 +0500

    Solution 1295. Find Numbers with Even Number of Digits

---
+++ Leetcode/Solution_1295.cs
@@ -0,0 +1,18 @@
+﻿// 1295. Find Numbers with Even Number of Digits
+public class Solution_1295
+{
+    public int FindNumbers(int[] nums)
+    {
+        var evenNumbersCount = 0;
+
+        foreach (var num in nums)
+        {
+            if (num.ToString().Length % 2 == 0)
+            {
+                evenNumbersCount++;
+            }
+        }
+
+        return evenNumbersCount;
+    }
+}
