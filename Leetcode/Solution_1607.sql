afb5d52768abb1b94d4fd685f1f71134630616b5
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:34:36 +0500

    Solution 1607. Sellers With No Sales

---
+++ Leetcode/Solution_1607.sql
@@ -0,0 +1,8 @@
+﻿-- 1607. Sellers With No Sales
+select
+    s.seller_name
+from Seller s
+    left join Orders o on s.seller_id = o.seller_id
+        and date_part('year', o.sale_date) = 2020
+where o.seller_id is null
+order by s.seller_name
