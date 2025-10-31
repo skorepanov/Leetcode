230b54d81498d75acbdd6c91c422e12d716aa3c6
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 17:45:26 +0500

    Solution 1907. Count Salary Categories

---
+++ Leetcode/Solution_1907.sql
@@ -0,0 +1,15 @@
+﻿-- 1907. Count Salary Categories
+select
+    'Low Salary' as category,
+    sum(case when income < 20000 then 1 else 0 end) as accounts_count
+from Accounts
+    union
+select
+    'Average Salary' as category,
+    sum(case when income between 20000 and 50000 then 1 else 0 end) as accounts_count
+from Accounts
+    union
+select
+    'High Salary' as category,
+    sum(case when income > 50000 then 1 else 0 end) as accounts_count
+from Accounts
