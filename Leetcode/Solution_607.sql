035ff93ec0a3f736300abc14a4160db86112ed6d
Sergey Korepanov <igzhestik@mail.ru>
2025-10-31 16:08:59 +0500

    Solution 607. Sales Person

---
+++ Leetcode/Solution_607.sql
@@ -0,0 +1,9 @@
+﻿-- 607. Sales Person
+select name
+from SalesPerson
+where sales_id not in (
+    select o.sales_id
+    from Orders o
+        inner join Company c on o.com_id = c.com_id
+    where c.name = 'RED'
+)
