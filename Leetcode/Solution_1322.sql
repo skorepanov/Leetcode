ea3d180ea8353dea858e23b0d8845a2e8608d3b3
Sergey Korepanov <igzhestik@mail.ru>
2025-11-04 19:39:17 +0500

    Solution 1322. Ads Performance

---
+++ Leetcode/Solution_1322.sql
@@ -0,0 +1,17 @@
+﻿-- 1322. Ads Performance
+with counts as (
+    select
+        ad_id,
+        count(*) filter (where action = 'Clicked') as clicked,
+        count(*) filter (where action = 'Viewed') as viewed
+    from Ads
+    group by ad_id
+)
+select
+    ad_id,
+    case
+        when (clicked + viewed) = 0 then 0
+        else round((clicked::decimal / (clicked + viewed)) * 100, 2)
+        end as ctr
+from counts
+order by ctr desc, ad_id asc
