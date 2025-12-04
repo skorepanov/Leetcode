2369ba05cd746bc8e4524b428e2dab010cd0ceb1
Sergey Korepanov <igzhestik@mail.ru>
2025-12-04 14:42:56 +0500

    Solution 547. Number of Provinces. Рефакторинг: Извлечён класс DisjointSet

--- Leetcode/Solution_547.cs
+++ Leetcode/Solution_547.cs
@@ -1,62 +1,78 @@
 ﻿// 547. Number of Provinces
 public class Solution_547
 {
-    private readonly Dictionary<int, int> _root = new ();
-    private readonly Dictionary<int, int> _rank = new ();
-
     public int FindCircleNum(int[][] isConnected)
     {
-        FillRootAndRank(isConnected);
-
-        var numberOfProvinces = _root.Count;
-        UnionNums(isConnected, ref numberOfProvinces);
-
-        return numberOfProvinces;
-    }
-
-    private void FillRootAndRank(int[][] isConnected)
-    {
-        for (var i = 0; i < isConnected.Length; i++)
+        if (isConnected is null || isConnected.Length == 0)
         {
-            _root.Add(i, i);
-            _rank.Add(i, 1);
-        }
-    }
-
-    private int FindRoot(int x)
-    {
-        if (x == _root[x])
-        {
-            return x;
+            return 0;
         }
 
-        _root[x] = FindRoot(_root[x]);
-
-        return _root[x];
+        var disjointSet = new DisjointSet(isConnected);
+        return disjointSet.Count;
     }
 
-    private void UnionNums(int[][] isConnected, ref int numberOfProvinces)
+    private class DisjointSet
     {
-        for (var i = 0; i < isConnected.Length; i++)
+        private readonly Dictionary<int, int> _root = new ();
+        private readonly Dictionary<int, int> _rank = new ();
+
+        public int Count { get; private set; }
+
+        public DisjointSet(int[][] isConnected)
         {
-            for (var j = i + 1; j < isConnected.Length; j++)
+            FillRootAndRank(isConnected);
+            UnionNums(isConnected);
+        }
+
+        private void FillRootAndRank(int[][] isConnected)
+        {
+            for (var i = 0; i < isConnected.Length; i++)
             {
-                if (isConnected[i][j] == 1 && FindRoot(i) != FindRoot(j))
+                _root.Add(i, i);
+                _rank.Add(i, 1);
+            }
+        }
+
+        private int FindRoot(int x)
+        {
+            if (x == _root[x])
+            {
+                return x;
+            }
+
+            _root[x] = FindRoot(_root[x]);
+
+            return _root[x];
+        }
+
+        private void UnionNums(int[][] isConnected)
+        {
+            Count = _root.Count;
+
+            for (var i = 0; i < isConnected.Length; i++)
+            {
+                for (var j = i + 1; j < isConnected.Length; j++)
                 {
-                    Union(i, j);
-                    numberOfProvinces--;
+                    if (isConnected[i][j] == 1 && FindRoot(i) != FindRoot(j))
+                    {
+                        Union(i, j);
+                        Count--;
+                    }
                 }
             }
         }
-    }
 
-    private void Union(int x, int y)
-    {
-        var rootX = FindRoot(x);
-        var rootY = FindRoot(y);
-
-        if (rootX != rootY)
+        private void Union(int x, int y)
         {
+            var rootX = FindRoot(x);
+            var rootY = FindRoot(y);
+
+            if (rootX == rootY)
+            {
+                return;
+            }
+
             if (_rank[rootX] > _rank[rootY])
             {
                 _root[rootY] = rootX;
