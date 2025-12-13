d4a8c2135f5fa59801364551d9bce2e8b382c770
Sergey Korepanov <igzhestik@mail.ru>
2025-12-13 21:42:31 +0500

    Solution 1584. Min Cost to Connect All Points

---
+++ Leetcode/Solution_1584.cs
@@ -0,0 +1,110 @@
+﻿// 1584. Min Cost to Connect All Points
+public class Solution_1584
+{
+    public int MinCostConnectPoints(int[][] points)
+    {
+        var distances = GetDistances(points);
+
+        var sortedDistances = distances
+            .OrderBy(d => d.MathDistance)
+            .ToList();
+
+        var addedEdgeCount = 0;
+        var distanceSum = 0m;
+        var pointGroups = new List<HashSet<Point>>();
+
+        foreach (var distance in sortedDistances)
+        {
+            if (addedEdgeCount == points.Length - 1)
+            {
+                break;
+            }
+
+            // если обе точки в одной группе - не добавлять точки
+            var areBothPointsInSingleGroup = pointGroups
+                .Any(g => g.Contains(distance.Point1) && g.Contains(distance.Point2));
+
+            if (areBothPointsInSingleGroup)
+            {
+                continue;
+            }
+
+            var groupForPoint1 = pointGroups
+                .FirstOrDefault(g => g.Contains(distance.Point1));
+            var groupForPoint2 = pointGroups
+                .FirstOrDefault(g => g.Contains(distance.Point2));
+
+            // если обе точки не добавлены - создать новую группу точек
+            if (groupForPoint1 is null && groupForPoint2 is null)
+            {
+                var newGroup = new HashSet<Point>
+                {
+                    distance.Point1,
+                    distance.Point2
+                };
+
+                pointGroups.Add(newGroup);
+            }
+            // если у первой точки есть группа, а у второй нет -
+            // добавить вторую точку в первую группу
+            else if (groupForPoint1 is not null && groupForPoint2 is null)
+            {
+                groupForPoint1.Add(distance.Point2);
+            }
+            // если у второй точки есть группа, а у первой нет -
+            // добавить первую точку во вторую группу
+            else if (groupForPoint1 is null && groupForPoint2 is not null)
+            {
+                groupForPoint2.Add(distance.Point1);
+            }
+            else
+            {
+                // если обе точки в разных группах -
+                // объединить группы
+                pointGroups.Remove(groupForPoint2);
+
+                foreach (var point in groupForPoint2)
+                {
+                    groupForPoint1.Add(point);
+                }
+            }
+
+            addedEdgeCount++;
+            distanceSum += distance.MathDistance;
+        }
+
+        return (int)distanceSum;
+    }
+
+    private List<Distance> GetDistances(int[][] points)
+    {
+        var distances = new List<Distance>();
+
+        for (var i = 0; i < points.Length; i++)
+        {
+            for (var j = 0; j < points.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                var point1 = new Point(points[i][0], points[i][1]);
+                var point2 = new Point(points[j][0], points[j][1]);
+
+                var mathDistance = Math.Abs(point1.X - point2.X)
+                                 + Math.Abs(point1.Y - point2.Y);
+
+                var distance = new Distance(point1, point2, mathDistance);
+
+                distances.Add(distance);
+            }
+        }
+
+        return distances;
+    }
+
+    private record Point(int X, int Y);
+
+    private record Distance(Point Point1, Point Point2, decimal MathDistance);
+}
