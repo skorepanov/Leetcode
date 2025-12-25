88f800894719c1ffd0ee55fea6db02636057900f
Sergey Korepanov <igzhestik@mail.ru>
2025-12-25 16:21:14 +0500

    Solution 1543. Fix Product Name Format

---
+++ Leetcode/Solution_1543.sql
@@ -0,0 +1,14 @@
+﻿-- 1543. Fix Product Name Format
+with s as (
+    select
+        lower(trim(product_name)) as product_name,
+        date_format(sale_date, '%Y-%m') as sale_date
+    from Sales
+)
+select
+    product_name,
+    sale_date,
+    count(*) as total
+from s
+group by product_name, sale_date
+order by product_name, sale_date
