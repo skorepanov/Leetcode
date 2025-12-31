78a6f3a447d23ec440eb81e79052d9c063f6cfd4
Sergey Korepanov <igzhestik@mail.ru>
2025-12-31 19:32:41 +0500

    Solution 1435. Create a Session Bar Chart

---
+++ Leetcode/Solution_1435.sql
@@ -0,0 +1,20 @@
+﻿-- 1435. Create a Session Bar Chart
+select
+    '[0-5>' as bin,
+    sum(case when 0 <= duration and duration < 5 * 60 then 1 else 0 end) as total
+from Sessions
+    union
+select
+    '[5-10>' as bin,
+    sum(case when 5 * 60 <= duration and duration < 10 * 60 then 1 else 0 end) as total
+from Sessions
+    union
+select
+    '[10-15>' as bin,
+    sum(case when 10 * 60 <= duration and duration < 15 * 60 then 1 else 0 end) as total
+from Sessions
+    union
+select
+    '15 or more' as bin,
+    sum(case when  15 * 60 <= duration then 1 else 0 end) as total
+from Sessions
