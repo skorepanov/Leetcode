6b916ea6d6884e918a0f7821b89ea8a3a48ecf34
Sergey Korepanov <igzhestik@mail.ru>
2026-01-17 21:27:10 +0500

    Solution 1565. Unique Orders and Customers Per Month

---
+++ Leetcode/Solution_1565.sql
@@ -0,0 +1,8 @@
+﻿-- 1565. Unique Orders and Customers Per Month
+select
+    to_char(order_date, 'YYYY-MM') as month,
+    count(order_id) as order_count,
+    count(distinct customer_id) as customer_count
+from Orders
+where invoice > 20
+group by month
