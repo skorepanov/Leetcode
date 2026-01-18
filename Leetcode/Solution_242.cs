def2e3e37450661bb40704c5e7f7b447bd2576ed
Sergey Korepanov <igzhestik@mail.ru>
2026-01-18 16:12:46 +0500

    Solution 242. Valid Anagram

---
+++ Leetcode/Solution_242.cs
@@ -0,0 +1,35 @@
+﻿// 242. Valid Anagram
+public class Solution_242
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        var symbols = new Dictionary<char, int>();
+
+        foreach (var symbol in s)
+        {
+            if (!symbols.ContainsKey(symbol))
+            {
+                symbols.Add(symbol, 0);
+            }
+
+            symbols[symbol]++;
+        }
+
+        foreach (var symbol in t)
+        {
+            if (!symbols.ContainsKey(symbol) || symbols[symbol] <= 0)
+            {
+                return false;
+            }
+
+            symbols[symbol]--;
+        }
+
+        return true;
+    }
+}
