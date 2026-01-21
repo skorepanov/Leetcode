c73a8a232512bed44da12bf01bee057d2f776ebd
Sergey Korepanov <igzhestik@mail.ru>
2026-01-21 22:44:51 +0500

    Solution 3217. Delete Nodes From Linked List Present in Array

---
+++ Leetcode/Solution_3217.cs
@@ -0,0 +1,37 @@
+﻿// 3217. Delete Nodes From Linked List Present in Array
+public class Solution_3217
+{
+    public ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        var numsToDelete = new HashSet<int>(nums);
+
+        var pseudoHead = new ListNode(val: 0, next: head);
+        var currentNode = pseudoHead;
+
+        while (currentNode.next is not null)
+        {
+            if (numsToDelete.Contains(currentNode.next.val))
+            {
+                currentNode.next = currentNode.next.next;
+            }
+            else
+            {
+                currentNode = currentNode.next;
+            }
+        }
+
+        return pseudoHead.next;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
