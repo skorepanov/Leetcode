d67b6ebd384a0fbf4f4f4ece649a85daf36ca0a4
Sergey Korepanov <igzhestik@mail.ru>
2026-01-21 22:33:38 +0500

    Solution 237. Delete Node in a Linked List

---
+++ Leetcode/Solution_237.cs
@@ -0,0 +1,21 @@
+﻿// 237. Delete Node in a Linked List
+public class Solution_237
+{
+    public void DeleteNode(ListNode node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
