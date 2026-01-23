e88a37e2247ba60e25cc8c1f66d18ac4ac0b0d06
Sergey Korepanov <igzhestik@mail.ru>
2026-01-23 17:55:59 +0500

    Solution 1623. All Valid Triplets That Can Represent a Country

---
+++ Leetcode/Solution_1623.sql
@@ -0,0 +1,11 @@
+﻿-- 1623. All Valid Triplets That Can Represent a Country
+select
+    a.student_name as member_A,
+    b.student_name as member_B,
+    c.student_name as member_C
+from SchoolA as a
+    cross join SchoolB as b
+    cross join SchoolC as c
+where a.student_id <> b.student_id and a.student_name <> b.student_name
+    and b.student_id <> c.student_id and b.student_name <> c.student_name
+    and a.student_id <> c.student_id and a.student_name <> c.student_name
