a1820c7b29ede0b7029d17a22fc3ed60d245a2ff
Sergey Korepanov <igzhestik@mail.ru>
2026-01-26 20:06:43 +0500

    Solution 221. Maximal Square

---
+++ Leetcode/Solution_221.cs
@@ -0,0 +1,40 @@
+﻿// 221. Maximal Square
+public class Solution_221
+{
+    public int MaximalSquare(char[][] matrix)
+    {
+        var rowCount = matrix.Length;
+        var columnCount = matrix[0].Length;
+
+        var dp = new int[rowCount + 1][];
+
+        for (var i = 0; i <= rowCount; i++)
+        {
+            dp[i] = new int[columnCount + 1];
+        }
+
+        var maxDpValue = 0;
+
+        for (var i = 1; i <= rowCount; i++)
+        {
+            for (var j = 1; j <= columnCount; j++)
+            {
+                if (matrix[i - 1][j - 1] != '1')
+                {
+                    continue;
+                }
+
+                dp[i][j] = new[]
+                {
+                    dp[i - 1][ j - 1],
+                    dp[i][j - 1],
+                    dp[i - 1][j]
+                }.Min() + 1;
+
+                maxDpValue = Math.Max(maxDpValue, dp[i][j]);
+            }
+        }
+
+        return maxDpValue * maxDpValue;
+    }
+}
