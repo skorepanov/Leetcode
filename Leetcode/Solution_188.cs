302303fac2f9053abd5ba8c573bd6c2eeff8925f
Sergey Korepanov <igzhestik@mail.ru>
2026-01-27 19:01:50 +0500

    Solution 188. Best Time to Buy and Sell Stock IV

---
+++ Leetcode/Solution_188.cs
@@ -0,0 +1,55 @@
+﻿// 188. Best Time to Buy and Sell Stock IV
+public class Solution_188
+{
+    private int[][][] _cache;
+    private int _k;
+    private int[] _prices;
+
+    public int MaxProfit(int k, int[] prices)
+    {
+        _k = k;
+        _prices = prices;
+
+        InitializeCache();
+
+        return Dp(i: 0, transactionRemaining: k, holding: 0);
+    }
+
+    private void InitializeCache()
+    {
+        _cache = new int[_prices.Length][][];
+
+        for (var i = 0; i < _cache.Length; i++)
+        {
+            _cache[i] = new int[_k + 1][];
+
+            for (var j = 0; j < _k + 1; j++)
+            {
+                _cache[i][j] = new int[2];
+            }
+        }
+    }
+
+    private int Dp(int i, int transactionRemaining, int holding)
+    {
+        if (transactionRemaining == 0 || i == _prices.Length)
+        {
+            return 0;
+        }
+
+        if (_cache[i][transactionRemaining][holding] != 0)
+        {
+            return _cache[i][transactionRemaining][holding];
+        }
+
+        var doNothing = Dp(i + 1, transactionRemaining, holding);
+
+        var doSomething = holding == 1
+            ? Dp(i + 1, transactionRemaining - 1, holding: 0) + _prices[i]
+            : Dp(i + 1, transactionRemaining, holding: 1) - _prices[i];
+
+        _cache[i][transactionRemaining][holding] = Math.Max(doNothing, doSomething);
+
+        return _cache[i][transactionRemaining][holding];
+    }
+}
