0ebf9a04aaecdc8d5a7cd4081936b78ceec112bf
Sergey Korepanov <igzhestik@mail.ru>
2026-01-27 16:53:00 +0500

    Solution 300. Longest Increasing Subsequence

---
+++ Leetcode/Solution_300.cs
@@ -0,0 +1,24 @@
+﻿// 300. Longest Increasing Subsequence
+public class Solution_300
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        var cache = new int[nums.Length];
+        Array.Fill(cache, value: 1);
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    cache[i] = Math.Max(cache[i], cache[j] + 1);
+                }
+            }
+        }
+
+        var longest = cache.Max();
+
+        return longest;
+    }
+}
