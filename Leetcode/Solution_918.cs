5d61e46cdc529031711fdc65c4d6a0f7f89da825
Sergey Korepanov <igzhestik@mail.ru>
2026-01-28 19:03:23 +0500

    Solution 918. Maximum Sum Circular Subarray

---
+++ Leetcode/Solution_918.cs
@@ -0,0 +1,34 @@
+﻿// 918. Maximum Sum Circular Subarray
+public class Solution_918
+{
+    public int MaxSubarraySumCircular(int[] nums)
+    {
+        var currentMax = 0;
+        var currentMin = 0;
+        var maxSum = nums[0];
+        var minSum = nums[0];
+        var totalSum = 0;
+
+        foreach (var num in nums)
+        {
+            // Normal Kadane's
+            currentMax = Math.Max(currentMax + num, num);
+            maxSum = Math.Max(maxSum, currentMax);
+
+            // Kadane's but with min to find minimum subarray
+            currentMin = Math.Min(currentMin + num, num);
+            minSum = Math.Min(minSum, currentMin);
+
+            totalSum += num;
+        }
+
+        // если минимальный подмассив содержит все элементы,
+        // например, если все элементы отрицательные
+        if (totalSum == minSum)
+        {
+            return maxSum;
+        }
+
+        return Math.Max(maxSum, totalSum - minSum);
+    }
+}
