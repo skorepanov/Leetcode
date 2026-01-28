91c4e4a6c549ebf8083bbc628b083eba5eccdee1
Sergey Korepanov <igzhestik@mail.ru>
2026-01-28 18:05:54 +0500

    Solution 121. Best Time to Buy and Sell Stock

---
+++ Leetcode/Solution_121.cs
@@ -0,0 +1,23 @@
+﻿// 121. Best Time to Buy and Sell Stock
+public class Solution_121
+{
+    public int MaxProfit(int[] prices)
+    {
+        var min = int.MaxValue;
+        var profit = 0;
+
+        foreach (var price in prices)
+        {
+            if (price < min)
+            {
+                min = price;
+            }
+            else if (price - min > profit)
+            {
+                profit = price - min;
+            }
+        }
+
+        return profit;
+    }
+}
