739011be87499f3d450c622eaf0b7bf924c4769c
Sergey Korepanov <igzhestik@mail.ru>
2026-01-28 17:02:27 +0500

    Solution 91. Decode Ways

---
+++ Leetcode/Solution_91.cs
@@ -0,0 +1,53 @@
+﻿// 91. Decode Ways
+public class Solution_91
+{
+    private string _s;
+    private int[] _cache;
+
+    public int NumDecodings(string s)
+    {
+        _s = s;
+
+        _cache = new int[s.Length];
+        Array.Fill(_cache, value: -1);
+
+        return Dp(i: 0);
+    }
+
+    private int Dp(int i)
+    {
+        if (i == _s.Length)
+        {
+            return 1;
+        }
+
+        if (_s[i] == '0')
+        {
+            return 0;
+        }
+
+        if (i == _s.Length - 1)
+        {
+            return 1;
+        }
+
+        if (_cache[i] != -1)
+        {
+            return _cache[i];
+        }
+
+        var count = Dp(i + 1);
+
+        var numberAsString = _s.Substring(startIndex: i, length: 2);
+        var number = int.Parse(numberAsString);
+
+        if (number <= 26)
+        {
+            count += Dp(i + 2);
+        }
+
+        _cache[i] = count;
+
+        return _cache[i];
+    }
+}
