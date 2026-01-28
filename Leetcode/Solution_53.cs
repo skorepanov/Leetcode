2ef84c06484b517b4586598387f4c0de6a55e1c7
Sergey Korepanov <igzhestik@mail.ru>
2026-01-28 17:35:21 +0500

    Solution 53. Maximum Subarray

---
+++ Leetcode/Solution_53.cs
@@ -0,0 +1,17 @@
+﻿// 53. Maximum Subarray
+public class Solution_53
+{
+    public int MaxSubArray(int[] nums)
+    {
+        var current = 0;
+        var max = int.MinValue;
+
+        foreach (var num in nums)
+        {
+            current = Math.Max(current + num, num);
+            max = Math.Max(max, current);
+        }
+
+        return max;
+    }
+}
