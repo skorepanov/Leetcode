cf8f9b926a700a0edb5c8e7eae374c56ed481a9b
Sergey Korepanov <igzhestik@mail.ru>
2026-01-29 21:55:14 +0500

    Solution 896. Monotonic Array

---
+++ Leetcode/Solution_896.cs
@@ -0,0 +1,33 @@
+﻿// 896. Monotonic Array
+public class Solution_896
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return true;
+        }
+
+        var isIncreasing = (bool?)null;
+        var isDecreasing = (bool?)null;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] < nums[i])
+            {
+                isDecreasing = false;
+            }
+            else if (nums[i - 1] > nums[i])
+            {
+                isIncreasing = false;
+            }
+
+            if (isIncreasing is not null && isDecreasing is not null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
