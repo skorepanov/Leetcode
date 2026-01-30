d8372c5b3c5f32d555823e28f60e50ac05a08b4c
Sergey Korepanov <igzhestik@mail.ru>
2026-01-30 16:57:16 +0500

    459. Repeated Substring Pattern

---
+++ Leetcode/Solution_459.cs
@@ -0,0 +1,30 @@
+﻿// 459. Repeated Substring Pattern
+public class Solution_459
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            var substring = s.Substring(startIndex: 0, length: i + 1);
+
+            var k = substring.Length;
+
+            while (k + substring.Length <= s.Length)
+            {
+                if (substring != s.Substring(startIndex: k, length: substring.Length))
+                {
+                    break;
+                }
+
+                k += substring.Length;
+            }
+
+            if (k >= s.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
