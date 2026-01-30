4c6e0d6dd47bec5232c61c93e0af5cfaeb0e8af9
Sergey Korepanov <igzhestik@mail.ru>
2026-01-30 17:39:22 +0500

    1502. Can Make Arithmetic Progression From Sequence

---
+++ Leetcode/Solution_1502.cs
@@ -0,0 +1,20 @@
+﻿// 1502. Can Make Arithmetic Progression From Sequence
+public class Solution_1502
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+
+        var diff = arr[1] - arr[0];
+
+        for (var i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != diff)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
