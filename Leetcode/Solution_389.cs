ce8be4de7fe6c1c3e520753a74e9e4634ee6dd98
Sergey Korepanov <igzhestik@mail.ru>
2026-01-30 16:32:04 +0500

    389. Find the Difference

---
+++ Leetcode/Solution_389.cs
@@ -0,0 +1,33 @@
+﻿// 389. Find the Difference
+public class Solution_389
+{
+    public char FindTheDifference(string s, string t)
+    {
+        var symbols = new Dictionary<char, int>();
+
+        foreach (var symbol in s)
+        {
+            if (!symbols.ContainsKey(symbol))
+            {
+                symbols.Add(symbol, value: 1);
+            }
+            else
+            {
+                symbols[symbol]++;
+            }
+        }
+
+        foreach (var symbol in t)
+        {
+            if (!symbols.ContainsKey(symbol)
+                || symbols[symbol] == 0)
+            {
+                return symbol;
+            }
+
+            symbols[symbol]--;
+        }
+
+        return ' ';
+    }
+}
