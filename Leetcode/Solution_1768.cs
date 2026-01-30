ca4cf7a84ba1b0263fc20750cd515b35128d0191
Sergey Korepanov <igzhestik@mail.ru>
2026-01-30 15:53:04 +0500

    1768. Merge Strings Alternately

---
+++ Leetcode/Solution_1768.cs
@@ -0,0 +1,27 @@
+﻿// 1768. Merge Strings Alternately
+using System.Text;
+
+public class Solution_1768
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        var mergedString = new StringBuilder();
+
+        var maxLength = Math.Max(word1.Length, word2.Length);
+
+        for (var i = 0; i < maxLength; i++)
+        {
+            if (i < word1.Length)
+            {
+                mergedString.Append(word1[i]);
+            }
+
+            if (i < word2.Length)
+            {
+                mergedString.Append(word2[i]);
+            }
+        }
+
+        return mergedString.ToString();
+    }
+}
