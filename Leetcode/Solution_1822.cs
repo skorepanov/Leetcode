bca00b014ce7798669ca4151212bcf1a06294f09
Sergey Korepanov <igzhestik@mail.ru>
2026-01-30 17:31:36 +0500

    1822. Sign of the Product of an Array

---
+++ Leetcode/Solution_1822.cs
@@ -0,0 +1,22 @@
+﻿// 1822. Sign of the Product of an Array
+public class Solution_1822
+{
+    public int ArraySign(int[] nums)
+    {
+        var isPositive = true;
+
+        foreach (var num in nums)
+        {
+            switch (num)
+            {
+                case 0:
+                    return 0;
+                case < 0:
+                    isPositive = !isPositive;
+                    break;
+            }
+        }
+
+        return isPositive ? 1 : -1;
+    }
+}
