cd2c70e1c93a4bf60e6915535634937f74ea686a
Sergey Korepanov <igzhestik@mail.ru>
2026-01-31 18:39:13 +0500

    682. Baseball Game

---
+++ Leetcode/Solution_682.cs
@@ -0,0 +1,41 @@
+﻿// 682. Baseball Game
+public class Solution_682
+{
+    public int CalPoints(string[] operations)
+    {
+        var record = new Stack<int>();
+
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case "+":
+                {
+                    var number1 = record.Pop();
+                    var number2 = record.Peek();
+
+                    record.Push(number1);
+                    record.Push(number1 + number2);
+                    continue;
+                }
+                case "D":
+                {
+                    var prevNumber = record.Peek();
+                    record.Push(prevNumber * 2);
+                    continue;
+                }
+                case "C":
+                    record.Pop();
+                    continue;
+                default:
+                {
+                    var newNumber = int.Parse(operation);
+                    record.Push(newNumber);
+                    break;
+                }
+            }
+        }
+
+        return record.Sum();
+    }
+}
