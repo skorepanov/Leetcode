0f84df9fd5f7e5d18a71f82a269d56024da20f86
Sergey Korepanov <igzhestik@mail.ru>
2026-01-31 18:57:09 +0500

    657. Robot Return to Origin

---
+++ Leetcode/Solution_657.cs
@@ -0,0 +1,22 @@
+﻿// 657. Robot Return to Origin
+public class Solution_657
+{
+    public bool JudgeCircle(string moves)
+    {
+        var x = 0;
+        var y = 0;
+
+        foreach (var move in moves)
+        {
+            switch (move)
+            {
+                case 'L': x--; break;
+                case 'R': x++; break;
+                case 'U': y++; break;
+                case 'D': y--; break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
