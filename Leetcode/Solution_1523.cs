478ed38106c8019c1d4295444afe438a63b17f39
Sergey Korepanov <igzhestik@mail.ru>
2026-02-02 15:24:19 +0500

    1523. Count Odd Numbers in an Interval Range

---
+++ Leetcode/Solution_1523.cs
@@ -0,0 +1,24 @@
+﻿// 1523. Count Odd Numbers in an Interval Range
+public class Solution_1523
+{
+    public int CountOdds(int low, int high)
+    {
+        var count = 0;
+
+        if (low % 2 == 1)
+        {
+            low++;
+            count++;
+        }
+
+        if (high % 2 == 1)
+        {
+            high--;
+            count++;
+        }
+
+        count += (high - low) / 2;
+
+        return count;
+    }
+}
