acd8135cbbdf2a8f44dd6c5cda296084722a5307
Sergey Korepanov <igzhestik@mail.ru>
2026-02-03 18:51:20 +0500

    976. Largest Perimeter Triangle

---
+++ Leetcode/Solution_976.cs
@@ -0,0 +1,18 @@
+﻿// 976. Largest Perimeter Triangle
+public class Solution_976
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        var orderedNums = nums.OrderByDescending(n => n).ToArray();
+
+        for (var i = 0; i < orderedNums.Length - 2; i++)
+        {
+            if (orderedNums[i] < orderedNums[i + 1] + orderedNums[i + 2])
+            {
+                return orderedNums[i] + orderedNums[i + 1] + orderedNums[i + 2];
+            }
+        }
+
+        return 0;
+    }
+}
