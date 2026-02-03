3f31103789ce5984c238569eba1f23afe429f308
Sergey Korepanov <igzhestik@mail.ru>
2026-02-03 19:07:12 +0500

    1232. Check If It Is a Straight Line

---
+++ Leetcode/Solution_1232.cs
@@ -0,0 +1,31 @@
+﻿// 1232. Check If It Is a Straight Line
+public class Solution_1232
+{
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+        if (coordinates.Length == 2)
+        {
+            return true;
+        }
+
+        var deltaX = GetDeltaX(coordinates[0], coordinates[1]);
+        var deltaY = GetDeltaY(coordinates[0], coordinates[1]);
+
+        for (var i = 2; i < coordinates.Length; i++)
+        {
+            if (deltaY * GetDeltaX(coordinates[0], coordinates[i])
+             != deltaX * GetDeltaY(coordinates[0], coordinates[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int GetDeltaX(int[] point1, int[] point2)
+        => point2[0] - point1[0];
+
+    private int GetDeltaY(int[] point1, int[] point2)
+        => point2[1] - point1[1];
+}
