0942595125c2e8ac7aa62108100e7ab795eb7386
Sergey Korepanov <igzhestik@mail.ru>
2026-02-05 22:05:54 +0500

    1056. Confusing Number

---
+++ Leetcode/Solution_1056.cs
@@ -0,0 +1,33 @@
+﻿// 1056. Confusing Number
+public class Solution_1056
+{
+    public bool ConfusingNumber(int n)
+    {
+        var tempN = n;
+        var rotatedN = 0;
+
+        var rotation = new Dictionary<int, int>
+        {
+            [0] = 0,
+            [1] = 1,
+            [6] = 9,
+            [8] = 8,
+            [9] = 6
+        };
+
+        while (tempN > 0)
+        {
+            var digit = tempN % 10;
+
+            if (!rotation.TryGetValue(digit, out var rotatedDigit))
+            {
+                return false;
+            }
+
+            rotatedN = rotatedN * 10 + rotatedDigit;
+            tempN /= 10;
+        }
+
+        return n != rotatedN;
+    }
+}
