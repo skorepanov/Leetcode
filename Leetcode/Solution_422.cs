831d7f18b27c9b1f07b8d1a4d92c1acc394425cb
Sergey Korepanov <igzhestik@mail.ru>
2026-02-05 20:57:47 +0500

    422. Valid Word Square

---
+++ Leetcode/Solution_422.cs
@@ -0,0 +1,39 @@
+﻿// 422. Valid Word Square
+public class Solution_422
+{
+    public bool ValidWordSquare(IList<string> words)
+    {
+        var length = words.Count;
+
+        if (words.Any(word => word.Length > length))
+        {
+            return false;
+        }
+
+        if (length == 1)
+        {
+            return true;
+        }
+
+        for (var row = 0; row < length; row++)
+        {
+            for (var column = row + 1; column < length; column++)
+            {
+                var symbol1 = words[row].Length > column
+                    ? words[row][column]
+                    : ' ';
+
+                var symbol2 = words[column].Length > row
+                    ? words[column][row]
+                    : ' ';
+
+                if (symbol1 != symbol2)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
