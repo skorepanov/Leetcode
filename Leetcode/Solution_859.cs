a437f9c870cbe52e7d2d048023c33f3002dc417f
Sergey Korepanov <igzhestik@mail.ru>
2026-02-05 20:23:52 +0500

    859. Buddy Strings

---
+++ Leetcode/Solution_859.cs
@@ -0,0 +1,49 @@
+﻿// 859. Buddy Strings
+public class Solution_859
+{
+    public bool BuddyStrings(string s, string goal)
+    {
+        if (s.Length != goal.Length)
+        {
+            return false;
+        }
+
+        if (s == goal)
+        {
+            return s.ToHashSet().Count != s.Length;
+        }
+
+        var indexOfFirstDiff = (int?)null;
+        var indexOfSecondDiff = (int?)null;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == goal[i])
+            {
+                continue;
+            }
+
+            if (indexOfFirstDiff is null)
+            {
+                indexOfFirstDiff = i;
+                continue;
+            }
+
+            if (indexOfSecondDiff is null)
+            {
+                indexOfSecondDiff = i;
+                continue;
+            }
+
+            return false;
+        }
+
+        if (indexOfSecondDiff is null)
+        {
+            return false;
+        }
+
+        return s[indexOfFirstDiff.Value] == goal[indexOfSecondDiff.Value]
+            && s[indexOfSecondDiff.Value] == goal[indexOfFirstDiff.Value];
+    }
+}
