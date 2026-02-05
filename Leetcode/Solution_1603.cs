c4b120e6834ada3703da3ce01bbd24ee8004d4a3
Sergey Korepanov <igzhestik@mail.ru>
2026-02-05 18:42:02 +0500

    1603. Design Parking System

---
+++ Leetcode/Solution_1603.cs
@@ -0,0 +1,26 @@
+﻿// 1603. Design Parking System
+public class ParkingSystem
+{
+    // 0 - big
+    // 1 - medium
+    // 2 - small
+    private readonly int[] _spaces = new int[3];
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        _spaces[0] = big;
+        _spaces[1] = medium;
+        _spaces[2] = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        if (_spaces[carType - 1] > 0)
+        {
+            _spaces[carType - 1]--;
+            return true;
+        }
+
+        return false;
+    }
+}
