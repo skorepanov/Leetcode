7fa117fed7c850325e68c1b22aef8656e0b49120
Sergey Korepanov <igzhestik@mail.ru>
2026-02-08 11:40:19 +0500

    1853. Convert Date Format

---
+++ Leetcode/Solution_1853.sql
@@ -0,0 +1,19 @@
+﻿-- 1853. Convert Date Format
+
+-- вариант 1
+-- Runtime 269 ms, Beats 45.16%
+select
+    to_char(day, 'FMDay')
+        || ', '
+        || to_char(day, 'FMMonth')
+        || ' '
+        || extract(day from day)::varchar
+        || ', '
+        || extract(year from day) as day
+from Days
+
+-- вариант 2
+-- Runtime 298 ms, Beats 22.58%
+select
+    to_char(day, 'FMDay, FMMonth FMDD, YYYY') as day
+from Days
