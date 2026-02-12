345647893382f33f8b16809b2e1a90bf9434f782
Sergey Korepanov <igzhestik@mail.ru>
2026-02-12 16:30:20 +0500

    1809. Ad-Free Sessions

---
+++ Leetcode/Solution_1809.sql
@@ -0,0 +1,7 @@
+﻿-- 1809. Ad-Free Sessions
+select
+    p.session_id
+from Playback p
+    left join Ads a on p.customer_id = a.customer_id
+        and a.timestamp between p.start_time and p.end_time
+where a.ad_id is null
