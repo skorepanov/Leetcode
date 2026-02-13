be27999ce214f3efc5f0fe4976798e6bb1b25de3
Sergey Korepanov <igzhestik@mail.ru>
2026-02-13 21:20:14 +0500

    461. Hamming Distance

---
+++ Leetcode/Solution_461.cs
@@ -0,0 +1,21 @@
+﻿// 461. Hamming Distance
+public class Solution_461
+{
+    public int HammingDistance(int x, int y)
+    {
+        var xor = x ^ y;
+        var distance = 0;
+
+        while (xor > 0)
+        {
+            if (xor % 2 == 1)
+            {
+                distance++;
+            }
+
+            xor = xor >> 1;
+        }
+
+        return distance;
+    }
+}
