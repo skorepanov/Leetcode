4c3b5dd6fc1168321d5c6a46e0d43d4ad893f327
Sergey Korepanov <igzhestik@mail.ru>
2026-02-13 18:13:05 +0500

    384. Shuffle an Array

---
+++ Leetcode/Solution_384.cs
@@ -0,0 +1,30 @@
+﻿// 384. Shuffle an Array
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.Reset();
+ * int[] param_2 = obj.Shuffle();
+ */
+
+public class Solution_384
+{
+    private int[] _originalNums;
+
+    public Solution_384(int[] nums)
+    {
+        _originalNums = nums;
+    }
+
+    public int[] Reset()
+    {
+        return _originalNums;
+    }
+
+    public int[] Shuffle()
+    {
+        var shuffledNums = _originalNums.ToArray();
+        Random.Shared.Shuffle(shuffledNums);
+        return shuffledNums;
+    }
+}
