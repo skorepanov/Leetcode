320244a0927a5409c7ddae2b8ee336c012bf235c
Sergey Korepanov <igzhestik@mail.ru>
2026-02-13 20:44:40 +0500

    204. Count Primes

---
+++ Leetcode/Solution_204.cs
@@ -0,0 +1,34 @@
+﻿// 204. Count Primes
+public class Solution_204
+{
+    /// <summary>
+    /// Sieve of Eratosthenes
+    /// </summary>
+    public int CountPrimes(int n)
+    {
+        if (n <= 2)
+        {
+            return 0;
+        }
+
+        var compounds = new bool[n];
+
+        compounds[0] = true;
+        compounds[1] = true;
+
+        for (var i = 2; i < n; i++)
+        {
+            if (compounds[i])
+            {
+                continue;
+            }
+
+            for (var j = 2; i * j < n; j++)
+            {
+                compounds[i * j] = true;
+            }
+        }
+
+        return compounds.Count(p => !p);
+    }
+}
