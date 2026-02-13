eea9897fdf596a7248a593b95f7373f72cea8293
Sergey Korepanov <igzhestik@mail.ru>
2026-02-13 21:05:11 +0500

    326. Power of Three

---
+++ Leetcode/Solution_326.cs
@@ -0,0 +1,18 @@
+﻿// 326. Power of Three
+public class Solution_326
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n == 0)
+        {
+            return false;
+        }
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+}
