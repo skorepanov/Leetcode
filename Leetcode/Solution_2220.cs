9ba165b83ee6ed8b846431f2d93f7be193fbd027
Sergey Korepanov <igzhestik@mail.ru>
2026-02-13 21:21:57 +0500

    2220. Minimum Bit Flips to Convert Number

---
+++ Leetcode/Solution_2220.cs
@@ -0,0 +1,21 @@
+﻿// 2220. Minimum Bit Flips to Convert Number
+public class Solution_2220
+{
+    public int MinBitFlips(int start, int goal)
+    {
+        var xor = start ^ goal;
+        var flipCount = 0;
+
+        while (xor > 0)
+        {
+            if (xor % 2 == 1)
+            {
+                flipCount++;
+            }
+
+            xor = xor >> 1;
+        }
+
+        return flipCount;
+    }
+}
