0a0ab1e7545d69d66dc003c6ae216ed199cbc0ac
Sergey Korepanov <igzhestik@mail.ru>
2026-02-17 22:44:16 +0500

    1677. Product's Worth Over Invoices

---
+++ Leetcode/Solution_1677.sql
@@ -0,0 +1,11 @@
+﻿-- 1677. Product's Worth Over Invoices
+select
+    p.name,
+    coalesce(sum(rest), 0) as rest,
+    coalesce(sum(paid), 0) as paid,
+    coalesce(sum(canceled), 0) as canceled,
+    coalesce(sum(refunded), 0) as refunded
+from Product p
+    left join Invoice i on p.product_id = i.product_id
+group by p.name
+order by p.name
