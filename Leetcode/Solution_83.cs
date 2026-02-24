0e4eb51dab7ed44758b95135ac8b81672809739d
Sergey Korepanov <igzhestik@mail.ru>
2026-02-24 18:55:07 +0500

    83. Remove Duplicates from Sorted List

---
+++ Leetcode/Solution_83.cs
@@ -0,0 +1,38 @@
+﻿// 83. Remove Duplicates from Sorted List
+public class Solution_83
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head is null || head.next is null)
+        {
+            return head;
+        }
+
+        var currentNode = head;
+
+        while (currentNode.next is not null)
+        {
+            if (currentNode.val == currentNode.next.val)
+            {
+                currentNode.next = currentNode.next.next;
+                continue;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return head;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next=null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
