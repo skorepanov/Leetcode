814fe8aea19aef83c5527673abdb2944385698c3
Sergey Korepanov <igzhestik@mail.ru>
2026-03-16 14:35:24 +0500

    1431. Kids With the Greatest Number of Candies

---
+++ Leetcode/Solution_1431.cs
@@ -0,0 +1,14 @@
+﻿// 1431. Kids With the Greatest Number of Candies
+public class Solution_1431
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var maxCandyCount = candies.Max();
+
+        var hasGreatestNumber = candies
+            .Select(c => c + extraCandies >= maxCandyCount)
+            .ToList();
+
+        return hasGreatestNumber;
+    }
+}
