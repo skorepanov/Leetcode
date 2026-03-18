9952a8fb7cb31643080cf7aa25b01431d5d89d8f
Sergey Korepanov <igzhestik@mail.ru>
2026-03-18 15:08:01 +0500

    2205. The Number of Users That Are Eligible for Discount

---
+++ Leetcode/Solution_2205.sql
@@ -0,0 +1,13 @@
+﻿-- 2205. The Number of Users That Are Eligible for Discount
+CREATE OR REPLACE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT AS $$
+BEGIN
+  RETURN (
+    select
+        count(distinct user_id)
+    from Purchases
+    where time_stamp >= startDate
+        and time_stamp between startDate and endDate
+        and amount >= minAmount
+  );
+END;
+$$ LANGUAGE plpgsql;
