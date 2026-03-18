d9872ba6dabe833264cc3e4062e8b5920a9499e5
Sergey Korepanov <igzhestik@mail.ru>
2026-03-18 15:13:27 +0500

    2230. The Users That Are Eligible for Discount

---
+++ Leetcode/Solution_2230.sql
@@ -0,0 +1,14 @@
+﻿-- 2230. The Users That Are Eligible for Discount
+CREATE OR REPLACE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+RETURNS TABLE (user_id INT) AS $$
+BEGIN
+  RETURN QUERY (
+      select distinct
+          p.user_id
+      from Purchases p
+      where time_stamp between startDate and endDate
+          and amount >= minAmount
+      order by p.user_id
+  );
+END;
+$$ LANGUAGE plpgsql;
