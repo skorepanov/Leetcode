051b5d53bdc7538e2d3d1a55dcd8795a86fed993
Sergey Korepanov <igzhestik@mail.ru>
2026-04-01 14:47:16 +0500

    574. Winning Candidate

---
+++ Leetcode/Solution_574.sql
@@ -0,0 +1,14 @@
+﻿-- 574. Winning Candidate
+with max_vote_id as (
+    select
+        v.candidateId,
+        count(*) as count
+    from Vote v
+    group by v.candidateId
+    order by count desc
+    limit 1
+)
+select
+    c.name
+from max_vote_id
+    inner join Candidate c on max_vote_id.candidateId = c.id
