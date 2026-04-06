1bd9887bc09bbe3019df834190393af9ec6b74ce
Sergey Korepanov <igzhestik@mail.ru>
2026-04-06 14:51:07 +0500

    2329. Product Sales Analysis V

---
+++ Leetcode/Solution_2329.sql
@@ -0,0 +1,10 @@
+﻿-- 2329. Product Sales Analysis V
+select
+    s.user_id,
+    sum(s.quantity * p.price) as spending
+from Sales s
+    inner join Product p on s.product_id = p.product_id
+group by s.user_id
+order by
+    spending desc,
+    s.user_id asc
