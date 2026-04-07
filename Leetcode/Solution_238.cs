13fdef0879defd46eaae211a269c3a6a56da20ec
Sergey Korepanov <igzhestik@mail.ru>
2026-04-07 17:22:42 +0500

    238. Product of Array Except Self

---
+++ Leetcode/Solution_238.cs
@@ -0,0 +1,31 @@
+﻿// 238. Product of Array Except Self
+public class Solution_238
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var prefixProducts = new int[nums.Length];
+        prefixProducts[0] = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            prefixProducts[i] = prefixProducts[i - 1] * nums[i - 1];
+        }
+
+        var suffixProducts = new int[nums.Length];
+        suffixProducts[^1] = 1;
+
+        for (var i = nums.Length - 2; i >= 0; i--)
+        {
+            suffixProducts[i] = suffixProducts[i + 1] * nums[i + 1];
+        }
+
+        var productsExpectSelf = new int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            productsExpectSelf[i] = prefixProducts[i] * suffixProducts[i];
+        }
+
+        return productsExpectSelf;
+    }
+}
