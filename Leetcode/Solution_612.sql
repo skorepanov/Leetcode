2c093c31ff2b121586110b942754897e1367d3a0
Sergey Korepanov <igzhestik@mail.ru>
2026-04-10 15:17:40 +0500

    612. Shortest Distance in a Plane

---
+++ Leetcode/Solution_612.sql
@@ -0,0 +1,5 @@
+﻿-- 612. Shortest Distance in a Plane
+select
+    min(round(sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2))::numeric, 2)) as shortest
+from Point2D p1
+    inner join Point2D p2 on p1.x != p2.x or p1.y != p2.y
