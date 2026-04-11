ba09c64fa3cbc6eb95d5605a4209ec617a79cd00
Sergey Korepanov <igzhestik@mail.ru>
2026-04-11 17:48:59 +0500

    2339. All the Matches of the League

---
+++ Leetcode/Solution_2339.sql
@@ -0,0 +1,6 @@
+﻿-- 2339. All the Matches of the League
+select
+    t1.team_name as home_team,
+    t2.team_name as away_team
+from Teams t1
+    inner join Teams t2 on t1.team_name != t2.team_name
