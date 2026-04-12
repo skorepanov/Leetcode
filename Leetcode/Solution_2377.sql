17cbdaf2310fa14c4bf1df1b102a4dcbaaa90169
Sergey Korepanov <igzhestik@mail.ru>
2026-04-12 17:59:20 +0500

    2377. Sort the Olympic Table

---
+++ Leetcode/Solution_2377.sql
@@ -0,0 +1,12 @@
+﻿-- 2377. Sort the Olympic Table
+select
+    country,
+    gold_medals,
+    silver_medals,
+    bronze_medals
+from Olympic
+order by
+    gold_medals desc,
+    silver_medals desc,
+    bronze_medals desc,
+    country asc
