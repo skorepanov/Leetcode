cc1ee86fa6b05948f46702636bbd8ff399c63690
Sergey Korepanov <igzhestik@mail.ru>
2026-04-13 15:42:45 +0500

    2480. Form a Chemical Bond

---
+++ Leetcode/Solution_2480.sql
@@ -0,0 +1,7 @@
+﻿-- 2480. Form a Chemical Bond
+select
+    e1.symbol as metal,
+    e2.symbol as nonmetal
+from Elements e1
+    inner join Elements e2
+        on e1.type = 'Metal' and e2.type = 'Nonmetal'
