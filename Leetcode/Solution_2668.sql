f730858304d564d5c8280400364ead53f1adf27a
Sergey Korepanov <igzhestik@mail.ru>
2026-04-14 15:24:31 +0500

    2668. Find Latest Salaries

---
+++ Leetcode/Solution_2668.sql
@@ -0,0 +1,11 @@
+﻿-- 2668. Find Latest Salaries
+select distinct on (emp_id)
+    emp_id,
+    firstname,
+    lastname,
+    salary,
+    department_id
+from Salary
+order by
+    emp_id asc,
+    salary desc
