9cdc18db60e48e11e06a5511618ee93fbe03be87
Sergey Korepanov <igzhestik@mail.ru>
2026-04-16 13:54:38 +0500

    614. Second Degree Follower

---
+++ Leetcode/Solution_614.sql
@@ -0,0 +1,8 @@
+﻿-- 614. Second Degree Follower
+select
+    f1.follower,
+    count(distinct f2.follower) as num
+from Follow f1
+    inner join Follow f2 on f1.follower = f2.followee
+group by f1.follower
+order by f1.follower
