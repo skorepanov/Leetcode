69227bee0908b4e6c45c0409ce879c7fa849cf42
Sergey Korepanov <igzhestik@mail.ru>
2026-04-19 17:26:41 +0500

    1098. Unpopular Books

---
+++ Leetcode/Solution_1098.sql
@@ -0,0 +1,10 @@
+﻿-- 1098. Unpopular Books
+select
+    b.book_id,
+    b.name
+from Books b
+    left join Orders o on b.book_id = o.book_id
+        and o.dispatch_date between '20180623' and '20190623'
+where b.available_from <= '20190523'
+group by b.book_id, b.name
+having coalesce(sum(o.quantity), 0) < 10
