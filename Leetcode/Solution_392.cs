ab8e888964f57a64c026c202e431066959e753d3
Sergey Korepanov <igzhestik@mail.ru>
2026-04-20 15:43:27 +0500

    392. Is Subsequence

---
+++ Leetcode/Solution_392.cs
@@ -0,0 +1,28 @@
+﻿// 392. Is Subsequence
+public class Solution_392
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        var sIndex = 0;
+
+        for (var tIndex = 0; tIndex < t.Length; tIndex++)
+        {
+            if (t[tIndex] == s[sIndex])
+            {
+                sIndex++;
+
+                if (sIndex == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
