259b8e8f5a8bb64d5ee26d69f803a91ee5bd5dc1
Sergey Korepanov <igzhestik@mail.ru>
2026-04-21 20:05:13 +0500

    1126. Active Businesses

---
+++ Leetcode/Solution_1126.sql
@@ -0,0 +1,15 @@
+﻿-- 1126. Active Businesses
+with middle as (
+    select
+        event_type,
+        avg(occurrences) as average
+    from Events
+    group by event_type
+)
+select
+    e.business_id
+from Events e
+    inner join middle m on e.event_type = m.event_type
+where e.occurrences > m.average
+group by e.business_id
+having count(*) > 1
