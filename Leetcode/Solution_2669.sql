b25c0af981a19a2b40967f023b51397485693d82
Sergey Korepanov <igzhestik@mail.ru>
2026-04-25 20:47:59 +0500

    2669. Count Artist Occurrences On Spotify Ranking List

---
+++ Leetcode/Solution_2669.sql
@@ -0,0 +1,9 @@
+﻿-- 2669. Count Artist Occurrences On Spotify Ranking List
+select
+    artist,
+    count(*) as occurrences
+from Spotify
+group by artist
+order by
+    occurrences desc,
+    artist asc
