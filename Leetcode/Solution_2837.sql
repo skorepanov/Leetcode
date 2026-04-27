75dcee742eb293756408c7c40ff8e100adef319f
Sergey Korepanov <igzhestik@mail.ru>
2026-04-27 11:52:51 +0500

    2837. Total Traveled Distance

---
+++ Leetcode/Solution_2837.sql
@@ -0,0 +1,9 @@
+﻿-- 2837. Total Traveled Distance
+select
+    u.user_id,
+    u.name,
+    coalesce(sum(r.distance), 0) as "traveled distance"
+from Users u
+    left join Rides r on u.user_id = r.user_id
+group by u.user_id, u.name
+order by u.user_id
