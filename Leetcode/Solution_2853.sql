8c54792ce28b0c183dcb6e3c925411e313afe4ee
Sergey Korepanov <igzhestik@mail.ru>
2026-04-27 12:00:58 +0500

    2853. Highest Salaries Difference

---
+++ Leetcode/Solution_2853.sql
@@ -0,0 +1,16 @@
+﻿-- 2853. Highest Salaries Difference
+with max_marketing as (
+    select
+        max(salary) as salary
+    from Salaries
+    where department = 'Marketing'
+),
+max_engineering as (
+    select
+        max(salary) as salary
+    from Salaries
+    where department = 'Engineering'
+)
+select
+    abs(max_marketing.salary - max_engineering.salary) as salary_difference
+from max_marketing, max_engineering
