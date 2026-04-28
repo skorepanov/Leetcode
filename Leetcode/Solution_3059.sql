a7a3a3dbc8d0cdb55ebfca79cb931aaaef81731e
Sergey Korepanov <igzhestik@mail.ru>
2026-04-28 16:41:25 +0500

    3059. Find All Unique Email Domains

---
+++ Leetcode/Solution_3059.sql
@@ -0,0 +1,8 @@
+﻿-- 3059. Find All Unique Email Domains
+select
+    split_part(email, '@', 2) as email_domain,
+    count(*) as count
+from Emails
+where email like '%.com'
+group by email_domain
+order by email_domain
