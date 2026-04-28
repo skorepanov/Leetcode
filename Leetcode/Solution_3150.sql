5f82bff186c201d1267c8f63b72d508df20ea4a8
Sergey Korepanov <igzhestik@mail.ru>
2026-04-28 22:33:10 +0500

    3150. Invalid Tweets II

---
+++ Leetcode/Solution_3150.sql
@@ -0,0 +1,8 @@
+﻿-- 3150. Invalid Tweets II
+select
+    tweet_id
+from Tweets
+where length(content) > 140
+    or regexp_count(content, '@') > 3
+    or regexp_count(content, '#') > 3
+order by tweet_id
