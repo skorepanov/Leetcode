3041bdcace131e8118490ef3465b576bd06479a1
Sergey Korepanov <igzhestik@mail.ru>
2026-04-28 16:32:44 +0500

    3053. Classifying Triangles by Lengths

---
+++ Leetcode/Solution_3053.sql
@@ -0,0 +1,9 @@
+﻿-- 3053. Classifying Triangles by Lengths
+select
+    case
+        when (a + b <= c) or (a + c <= b) or (b + c <= a) then 'Not A Triangle'
+        when a = b and b = c then 'Equilateral'
+        when a = b or a = c or b = c then 'Isosceles'
+        else 'Scalene'
+    end as triangle_type
+from Triangles
