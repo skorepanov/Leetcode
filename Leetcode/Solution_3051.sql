d9a3ce40a7703dea24d8c83cbe667a43a585b6a7
Sergey Korepanov <igzhestik@mail.ru>
2026-04-28 16:13:48 +0500

    3051. Find Candidates for Data Scientist Position

---
+++ Leetcode/Solution_3051.sql
@@ -0,0 +1,8 @@
+﻿-- 3051. Find Candidates for Data Scientist Position
+select
+    candidate_id
+from Candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill) = 3
+order by candidate_id
