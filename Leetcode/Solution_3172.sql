30ec49829abe6beb364c60d8d6543309583a0bb4
Sergey Korepanov <igzhestik@mail.ru>
2026-04-29 16:20:47 +0500

    3172. Second Day Verification

---
+++ Leetcode/Solution_3172.sql
@@ -0,0 +1,8 @@
+﻿-- 3172. Second Day Verification
+select
+    e.user_id
+from emails e
+    inner join texts t on e.email_id = t.email_id
+where t.signup_action = 'Verified'
+    and (e.signup_date + interval '1 day')::date = t.action_date::date
+order by e.user_id
