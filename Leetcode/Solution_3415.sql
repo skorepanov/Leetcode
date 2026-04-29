4826a9b56f077ba0ff45e55fefb969037ac5749d
Sergey Korepanov <igzhestik@mail.ru>
2026-04-29 16:51:43 +0500

    3415. Find Products with Three Consecutive Digits

---
+++ Leetcode/Solution_3415.sql
@@ -0,0 +1,8 @@
+﻿-- 3415. Find Products with Three Consecutive Digits
+select
+    product_id,
+    name
+from Products
+where regexp_like(name, '\d{3}')
+    and not regexp_like(name, '\d{4,}')
+order by product_id
