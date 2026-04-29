524a5dc84643ebf5f4403673c03964a3d66268d7
Sergey Korepanov <igzhestik@mail.ru>
2026-04-29 16:35:25 +0500

    3246. Premier League Table Ranking

---
+++ Leetcode/Solution_3246.sql
@@ -0,0 +1,10 @@
+﻿-- 3246. Premier League Table Ranking
+select
+    team_id,
+    team_name,
+    wins * 3 + draws as points,
+    rank() over (order by wins * 3 + draws desc) as position
+from TeamStats
+order by
+    points desc,
+    team_name asc
