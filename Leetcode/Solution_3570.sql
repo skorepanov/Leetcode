e4ff17d8873cf248fa8ba38cf0c2db81b70e2c00
Sergey Korepanov <igzhestik@mail.ru>
2026-04-30 22:05:57 +0500

    3570. Find Books with No Available Copies

---
+++ Leetcode/Solution_3570.sql
@@ -0,0 +1,22 @@
+﻿-- 3570. Find Books with No Available Copies
+with borrowed_books as (
+    select
+        book_id,
+        count(record_id) as current_borrowers
+    from borrowing_records
+    where return_date is null
+    group by book_id
+)
+select
+    lb.book_id,
+    lb.title,
+    lb.author,
+    lb.genre,
+    lb.publication_year,
+    bb.current_borrowers
+from borrowed_books bb
+    inner join library_books lb on bb.book_id = lb.book_id
+where lb.total_copies = bb.current_borrowers
+order by
+    bb.current_borrowers desc,
+    lb.title asc
