1ac57f2646bf96137ea8dfd1f00e326020d8c621
Sergey Korepanov <igzhestik@mail.ru>
2026-04-30 21:46:17 +0500

    3465. Find Products with Valid Serial Numbers

---
+++ Leetcode/Solution_3465.sql
@@ -0,0 +1,8 @@
+﻿-- 3465. Find Products with Valid Serial Numbers
+select
+    product_id,
+    product_name,
+    description
+from products
+where regexp_like(description, '( |^)SN\d{4}-\d{4}( |$)')
+order by product_id
