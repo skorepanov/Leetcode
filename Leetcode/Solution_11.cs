3ba4ea6b5b109b427a7b911306957213f5161275
Sergey Korepanov <igzhestik@mail.ru>
2026-05-01 19:57:12 +0500

    11. Container With Most Water

---
+++ Leetcode/Solution_11.cs
@@ -0,0 +1,44 @@
+﻿// 11. Container With Most Water
+public class Solution_11
+{
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var maxVolume = 0;
+
+        while (left < right)
+        {
+            if (height[left] == 0)
+            {
+                left++;
+                continue;
+            }
+
+            if (height[right] == 0)
+            {
+                right--;
+                continue;
+            }
+
+            var currentHeight = Math.Min(height[left], height[right]);
+            var currentVolume = currentHeight * (right - left);
+
+            if (currentVolume > maxVolume)
+            {
+                maxVolume = currentVolume;
+            }
+
+            if (height[left] <= height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxVolume;
+    }
+}
