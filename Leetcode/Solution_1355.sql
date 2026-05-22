0604ff62764ec372e362028fdb3246afa2d7ea9d
Sergey Korepanov <igzhestik@mail.ru>
2026-05-22 17:14:05 +0500

    1355. Activity Participants

---
+++ Leetcode/Solution_1355.sql
@@ -0,0 +1,13 @@
+﻿-- 1355. Activity Participants
+with counts as (
+    select
+        activity,
+        count(id) as count
+    from Friends
+    group by activity
+)
+select
+    activity
+from counts
+where count <> (select min(count) from counts)
+    and count <> (select max(count) from counts)
