bc45bebbb2d639dcf62296d5778f7b126dd4e5aa
Sergey Korepanov <igzhestik@mail.ru>
2026-08-10 21:57:31 +0500

    1613. Find the Missing IDs

---
+++ Leetcode/Solution_1613.sql
@@ -0,0 +1,19 @@
+﻿-- 1613. Find the Missing IDs
+with all_ids as (
+    select
+        ids
+    from generate_series(
+        1,
+        (select
+            max(customer_id)
+        from Customers)
+    ) as ids
+)
+select
+    ids
+from all_ids
+    except
+select
+    customer_id
+from Customers
+order by ids
