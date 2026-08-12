ffa5f072e0eac61807c1a78b2869874322d9f89e
Sergey Korepanov <igzhestik@mail.ru>
2026-08-13 00:07:31 +0500

    1715. Count Apples and Oranges

---
+++ Leetcode/Solution_1715.sql
@@ -0,0 +1,6 @@
+﻿-- 1715. Count Apples and Oranges
+select
+    sum(b.apple_count + coalesce(c.apple_count, 0)) as apple_count,
+    sum(b.orange_count + coalesce(c.orange_count, 0)) as orange_count
+from Boxes b
+    left join Chests c on b.chest_id = c.chest_id
